dbf5d277270e139cad3853de85c54ebb6024f2a3
Chester Kener <Cl3Kener@gmail.com>
2014-01-31 17:32:19 -0500

    Put some SNAP into it!
    Signed-off-by: Chester Kener <Cl3Kener@gmail.com>
    Signed-off-by: Chet Kener <Cl3Kener@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/com/android/launcher3/DragController.java
+++ src/com/android/launcher3/DragController.java
@@ -53,7 +53,7 @@ public class DragController {
     /** Indicates the drag is a copy.  */
     public static int DRAG_ACTION_COPY = 1;
 
-    public static final int SCROLL_DELAY = 500;
+    public static final int SCROLL_DELAY = 350;
     public static final int RESCROLL_DELAY = PagedView.PAGE_SNAP_ANIMATION_DURATION + 150;
 
     private static final boolean PROFILE_DRAWING_DURING_DRAG = false;
