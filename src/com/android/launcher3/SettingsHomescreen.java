shagbag913/packages_apps_Launcher3

add search provider option (Google, Bing, DuckDuckGo)
Bootleggers (eldainosor) edit: Let's use it on non-Google app search

src/com/android/launcher3/SettingsHomescreen.java
@@ -152,6 +152,21 @@ public class SettingsHomescreen extends SettingsActivity implements PreferenceFr
                     return true;
                 }
             });
+        
+            ListPreference searchProvider = (ListPreference) findPreference(Utilities.SEARCH_PROVIDER_KEY);
+            if (BootlegUtils.isPackageInstalled(mContext, LauncherTab.SEARCH_PACKAGE)) {
+                getPreferenceScreen().removePreference(searchProvider);
+            } else {
+                searchProvider.setSummary(searchProvider.getEntry());
+                searchProvider.setOnPreferenceChangeListener(new OnPreferenceChangeListener() {
+                    public boolean onPreferenceChange(Preference preference, Object newValue) {
+                        int index = searchProvider.findIndexOfValue((String) newValue);
+                        searchProvider.setSummary(searchProvider.getEntries()[index]);
+                        LauncherAppState.getInstanceNoCreate().setNeedsRestart();
+                        return true;
+                    }
+                });
+            }
         }
 
         @Override

crdroidandroid/android_packages_apps_Launcher3

Signed-off-by: spezi77 <spezi7713@gmx.net>

src/com/android/launcher3/SettingsHomescreen.java
@@ -152,6 +152,21 @@ public class SettingsHomescreen extends SettingsActivity implements PreferenceFr
                     return true;
                 }
             });
+        
+            ListPreference searchProvider = (ListPreference) findPreference(Utilities.SEARCH_PROVIDER_KEY);
+            if (PixeldustUtils.isPackageInstalled(mContext, LauncherTab.SEARCH_PACKAGE)) {
+                getPreferenceScreen().removePreference(searchProvider);
+            } else {
+                searchProvider.setSummary(searchProvider.getEntry());
+                searchProvider.setOnPreferenceChangeListener(new OnPreferenceChangeListener() {
+                    public boolean onPreferenceChange(Preference preference, Object newValue) {
+                        int index = searchProvider.findIndexOfValue((String) newValue);
+                        searchProvider.setSummary(searchProvider.getEntries()[index]);
+                        LauncherAppState.getInstanceNoCreate().setNeedsRestart();
+                        return true;
+                    }
+                });
+            }
         }
 
         @Override