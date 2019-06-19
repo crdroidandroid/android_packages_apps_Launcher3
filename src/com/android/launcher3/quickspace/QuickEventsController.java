a919f52f930672545de1ce3af372973c2e2bcc56
ElDainosor <eldainosor@gmail.com>
2019-06-19 05:15:19 -0300

    Launcher3: Let's bring back device introduction action
    Now that we have an activity
    Also, let's randomize the surprise title
    
    Change-Id: Iee493fb32ba361b03a5642ba22e44a890568a92f
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/com/android/launcher3/quickspace/QuickEventsController.java
+++ src/com/android/launcher3/quickspace/QuickEventsController.java
@@ -30,6 +30,7 @@ import com.android.launcher3.Launcher;
 import com.android.launcher3.R;
 
 import java.util.List;
+import java.util.Random;
 import java.util.ArrayList;
 
 public class QuickEventsController {
@@ -46,13 +47,12 @@ public class QuickEventsController {
     private boolean mIsQuickEvent = false;
 
     // Device Intro
-    /**private boolean mEventIntro = false;
+    private boolean mEventIntro = false;
     private boolean mEventIntroClicked = false;
-    private boolean mIsFirstTimeDone; **/
+    private boolean mIsFirstTimeDone; 
      /** Ambient Play
     private boolean mEventAmbientPlay = false;
     private long mLastAmbientInfo;
-gi
     private BroadcastReceiver mAmbientReceiver = new BroadcastReceiver() {
         @Override
         public void onReceive(Context context, Intent intent) {
@@ -72,12 +72,12 @@ gi
     }
 
     public void initQuickEvents() {
-        /**mIsFirstTimeDone = Settings.System.getInt(mContext.getContentResolver(), SETTING_DEVICE_INTRO_COMPLETED, 0) != 0;
+        mIsFirstTimeDone = Settings.System.getInt(mContext.getContentResolver(), SETTING_DEVICE_INTRO_COMPLETED, 0) != 0;
         deviceIntroEvent();
-        ambientPlayEvent();**/
+        //ambientPlayEvent();
     }
 
-    /**private void deviceIntroEvent() {
+    private void deviceIntroEvent() {
         if (mIsFirstTimeDone || mEventIntroClicked) {
             mEventIntro = false;
             return;
@@ -85,12 +85,19 @@ gi
         mIsQuickEvent = true;
         mEventIntro = true;
         mEventTitle = mContext.getResources().getString(R.string.quick_event_rom_intro_welcome);
-        mEventTitleSub = mContext.getResources().getString(R.string.quick_event_rom_intro_learn);
+        int mRandomLuck = getLuckyNumber(10);
+        String endingWelcome;
+        if (mRandomLuck == 7) {
+            endingWelcome = mContext.getResources().getStringArray(R.array.welcome_message_variants)[getLuckyNumber(0,6)];
+        } else {
+            endingWelcome = mContext.getResources().getStringArray(R.array.welcome_message_variants)[0];
+        }
+        mEventTitleSub = mContext.getResources().getString(R.string.quick_event_rom_intro_bridge, endingWelcome);
 
         mEventTitleSubAction = new OnClickListener() {
             @Override
             public void onClick(View view) {
-                final Intent intent = new Intent(Settings.ACTION_DEVICE_INTRODUCTION)
+                final Intent intent = new Intent(Intent.ACTION_MAIN).setClassName("com.android.settings","com.bootleggers.dumpster.fragments.BootlegWelcome")
                         .addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_RESET_TASK_IF_NEEDED);
                 try {
                     Launcher.getLauncher(mContext).startActivitySafely(view, intent, null);
@@ -100,7 +107,7 @@ gi
                 mEventIntroClicked = true;
             }
         };
-    } **/
+    }
 
     /**public void ambientPlayEvent() {
         if (mEventAmbientPlay) {
@@ -158,5 +165,14 @@ gi
 
     public int getActionIcon() {
         return mEventSubIcon;
+    }
+
+    public int getLuckyNumber(int max) {
+        return getLuckyNumber(0, max);
+    }
+
+    public int getLuckyNumber(int min, int max) {
+        Random r = new Random();
+        return r.nextInt((max - min) + 1) + min;
     }
 }
