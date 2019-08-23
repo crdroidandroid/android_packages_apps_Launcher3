dfeeba70801f3758665ff557ceb8498cd5a15413
El Dainosor <eldainosor@gmail.com>
2019-08-23 05:33:33 +0000

    Launcher3: Add alternate Quickspace layout
    just for the funnies
    
    Change-Id: I8796033593d0b7e28cea4bfb8c6f5e3b1a432ee4
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/com/android/launcher3/Utilities.java
+++ src/com/android/launcher3/Utilities.java
@@ -125,6 +125,7 @@ public final class Utilities {
     private static final long WAIT_BEFORE_RESTART = 250;
 
     public static final String DESKTOP_SHOW_QUICKSPACE = "pref_show_quickspace";
+    public static final String KEY_SHOW_ALT_QUICKSPACE = "pref_show_alt_quickspace";
 
     public static final String SLEEP_GESTURE = "pref_sleep_gesture";
 
@@ -728,5 +729,9 @@ public final class Utilities {
     public static boolean showQuickspace(Context context) {
         SharedPreferences prefs = getPrefs(context.getApplicationContext());
         return prefs.getBoolean(DESKTOP_SHOW_QUICKSPACE, true);
+    }
+
+    public static boolean useAlternativeQuickspaceUI(Context context) {
+        return getPrefs(context).getBoolean(KEY_SHOW_ALT_QUICKSPACE, false);
     }
 }
