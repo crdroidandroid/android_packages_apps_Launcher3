1fadb6665a616b9bb95c2d0022bae4dae29c7c67
Amir Zaidi <azaidi@live.nl>
2020-01-09 19:40:28 +0100

    Launcher3: Add AppInfo Bottom Sheet from Shade Launcher
    Shade: Rename ShortcutFactory to ShadeShortcutFactory
    
    Shade: Override AppInfoShortcut with BottomSheetShortcut
    
    Shade: Import bottom sheet class from Pie
    
    Shade: Fix ShadeShortcutFactory constructor
    
    Shade: Add testing entries to App Info
    
    Shade: Add real entries to App Info
    
    Shade: Implement parsing for app info
    
    Shade: Move all preference title strings to global strings file
    
    Shade: Set source to "Unknown" if it is empty
    
    Shade: Set icon pack to "None" by default
    
    Shade: Update app info bottom sheet to Q widgets bottom sheet
    
    Shade: Disable overscroll effect on bottom sheet preferences
    
    Shade: Update Version app info preference icon
    
    Shade: Reorder app info preference entries
    
    DennySPb: adapt to 11 without onClickListener proxy
    
    Change-Id: I34fe46944561723d3a47db92b3814e1deee80b5b
    Signed-off-by: DennySPb <dennyspb@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

---
+++ src/com/android/launcher3/customization/InfoBottomSheet.java
@@ -0,0 +1,151 @@
+package com.android.launcher3.customization;
+
+import android.app.ActivityOptions;
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.content.ComponentName;
+import android.content.Context;
+import android.os.Bundle;
+import androidx.preference.Preference;
+import androidx.preference.PreferenceFragment;
+import androidx.recyclerview.widget.RecyclerView;
+import android.graphics.Rect;
+
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.android.launcher3.BaseDraggingActivity;
+import com.android.launcher3.model.data.ItemInfo;
+import com.android.launcher3.Launcher;
+import com.android.launcher3.R;
+import com.android.launcher3.util.ComponentKey;
+import com.android.launcher3.widget.WidgetsBottomSheet;
+import com.android.launcher3.util.PackageManagerHelper;
+
+public class InfoBottomSheet extends WidgetsBottomSheet {
+    private final FragmentManager mFragmentManager;
+    protected static Rect mSourceBounds;
+    protected static BaseDraggingActivity mTarget;
+
+    public InfoBottomSheet(Context context) {
+        this(context, null);
+    }
+
+    public InfoBottomSheet(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public InfoBottomSheet(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        mFragmentManager = Launcher.getLauncher(context).getFragmentManager();
+    }
+
+    public void configureBottomSheet(Rect sourceBounds, BaseDraggingActivity target) {
+        mSourceBounds = sourceBounds;
+        mTarget = target;
+    }
+
+    @Override
+    public void populateAndShow(ItemInfo itemInfo) {
+        super.populateAndShow(itemInfo);
+        TextView title = findViewById(R.id.title);
+        title.setText(itemInfo.title);
+
+        PrefsFragment fragment =
+                (PrefsFragment) mFragmentManager.findFragmentById(R.id.sheet_prefs);
+        fragment.loadForApp(itemInfo);
+    }
+
+    @Override
+    public void onDetachedFromWindow() {
+        Fragment pf = mFragmentManager.findFragmentById(R.id.sheet_prefs);
+        if (pf != null) {
+            mFragmentManager.beginTransaction()
+                    .remove(pf)
+                    .commitAllowingStateLoss();
+        }
+        super.onDetachedFromWindow();
+    }
+
+    @Override
+    public void onWidgetsBound() {
+    }
+
+    public static class PrefsFragment extends PreferenceFragment
+            implements Preference.OnPreferenceChangeListener, Preference.OnPreferenceClickListener {
+        private static final String KEY_ICON_PACK = "pref_app_info_icon_pack";
+        private static final String KEY_SOURCE = "pref_app_info_source";
+        private static final String KEY_LAST_UPDATE = "pref_app_info_last_update";
+        private static final String KEY_VERSION = "pref_app_info_version";
+        private static final String KEY_MORE = "pref_app_info_more";
+
+        private Context mContext;
+
+        private ItemInfo mItemInfo;
+
+        private ComponentName mComponent;
+        private ComponentKey mKey;
+
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            mContext = getActivity();
+        }
+
+        @Override
+        public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+            addPreferencesFromResource(R.xml.app_info_preferences);
+        }
+
+        @Override
+        public RecyclerView onCreateRecyclerView(LayoutInflater inflater, ViewGroup parent,
+                                                 Bundle savedInstanceState) {
+            RecyclerView view = super.onCreateRecyclerView(inflater, parent, savedInstanceState);
+            view.setOverScrollMode(View.OVER_SCROLL_NEVER);
+            return view;
+        }
+
+        public void loadForApp(ItemInfo itemInfo) {
+            mComponent = itemInfo.getTargetComponent();
+            mItemInfo = itemInfo;
+            mKey = new ComponentKey(mComponent, itemInfo.user);
+//            mOnMoreClick = onMoreClick;
+            MetadataExtractor extractor = new MetadataExtractor(mContext, mComponent);
+
+            Preference iconPack = findPreference(KEY_ICON_PACK);
+            iconPack.setOnPreferenceChangeListener(this);
+            iconPack.setSummary(R.string.app_info_icon_pack_none);
+            findPreference(KEY_SOURCE).setSummary(extractor.getSource());
+            findPreference(KEY_LAST_UPDATE).setSummary(extractor.getLastUpdate());
+            findPreference(KEY_VERSION).setSummary(mContext.getString(
+                    R.string.app_info_version_value,
+                    extractor.getVersionName(),
+                    extractor.getVersionCode()));
+            findPreference(KEY_MORE).setOnPreferenceClickListener(this);
+        }
+
+        @Override
+        public boolean onPreferenceChange(Preference preference, Object newValue) {
+            if (KEY_ICON_PACK.equals(preference.getKey())) {
+                // Reload in launcher.
+            }
+            return false;
+        }
+
+        private void onMoreClick() {
+            new PackageManagerHelper(InfoBottomSheet.mTarget).startDetailsActivityForInfo(
+                        mItemInfo, InfoBottomSheet.mSourceBounds, ActivityOptions.makeBasic().toBundle());
+        }
+
+        @Override
+        public boolean onPreferenceClick(Preference preference) {
+            if (KEY_MORE.equals(preference.getKey())) {
+                  onMoreClick();
+            }
+            return false;
+        }
+    }
+}
