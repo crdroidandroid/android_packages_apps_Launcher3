07e3e603bb655abaf966fb215bdc23c1d7178bd6
ElDainosor <eldainosor@gmail.com>
2020-01-14 10:14:02 -0800

    Launcher3: Workaround the Icon pack issue on adaptive apps
    On the previous icon pack commit, there was an issue regarding icon packs and adaptive icons. When an app that got an icon targeted as SDK +26 (adaptive-ready) and got a custom icon from a icon pack, the icon would take the new icon as foreground and it'll take the generic white adaptive background. (see before photo to see it in action)
    Now, with this commit, if the icon packs supports the respective icon, it'll apply just the icon image instead of doing that. It's not the best approach, but it's a quick workaround till there's a better method. (see after)
    
    * statix edits
    - update for 10 by removing code to generate the adaptive drawable in BaseIconFactory, going along with the way we do icon packs already.
    
    Change-Id: Ie639d056413a13897acd2dc1eeb6c5632a0aa645
    Before: https://i.imgur.com/nlqBVlQ.png?1
    After: https://i.imgur.com/HIkcg5D.png?1
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- iconloaderlib/src/com/android/launcher3/icons/BaseIconFactory.java
+++ iconloaderlib/src/com/android/launcher3/icons/BaseIconFactory.java
@@ -213,30 +213,7 @@ public class BaseIconFactory implements AutoCloseable {
         if (icon == null) {
             return null;
         }
-        float scale = 1f;
-
-        if (shrinkNonAdaptiveIcons && ATLEAST_OREO) {
-            if (mWrapperIcon == null) {
-                mWrapperIcon = mContext.getDrawable(R.drawable.adaptive_icon_drawable_wrapper)
-                        .mutate();
-            }
-            AdaptiveIconDrawable dr = (AdaptiveIconDrawable) mWrapperIcon;
-            dr.setBounds(0, 0, 1, 1);
-            boolean[] outShape = new boolean[1];
-            scale = getNormalizer().getScale(icon, outIconBounds, dr.getIconMask(), outShape);
-            if (!(icon instanceof AdaptiveIconDrawable) && !outShape[0]) {
-                FixedScaleDrawable fsd = ((FixedScaleDrawable) dr.getForeground());
-                fsd.setDrawable(icon);
-                fsd.setScale(scale);
-                icon = dr;
-                scale = getNormalizer().getScale(icon, outIconBounds, null, null);
-
-                ((ColorDrawable) dr.getBackground()).setColor(mWrapperBackgroundColor);
-            }
-        } else {
-            scale = getNormalizer().getScale(icon, outIconBounds, null, null);
-        }
-
+        float scale = getNormalizer().getScale(icon, outIconBounds, null, null);
         outScale[0] = scale;
         return icon;
     }
