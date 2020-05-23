80fc533f851a7fc2ff530eb412d229fd86d22525
Pranav Vashi <neobuddy89@gmail.com>
2020-05-24 04:34:13 +0530

    Launcher3: Fix IntentFilter leak in QuickEventsController
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/com/android/launcher3/quickspace/QuickEventsController.java
+++ src/com/android/launcher3/quickspace/QuickEventsController.java
@@ -50,7 +50,8 @@ public class QuickEventsController {
 
     private boolean mIsQuickEvent = false;
     private boolean mImportantQuickEvent = false;
-    private boolean mRunning;
+    private boolean mRunning = true;
+    private boolean mRegistered = false;
 
     // Device Intro
     private boolean mEventIntro = false;
@@ -95,12 +96,24 @@ public class QuickEventsController {
     public void initQuickEvents() {
         mPreferences = mContext.getSharedPreferences(LauncherFiles.SHARED_PREFERENCES_KEY, Context.MODE_PRIVATE);
         mIsFirstTimeDone = mPreferences.getBoolean(SETTING_DEVICE_INTRO_COMPLETED, false);
+        registerPSAListener();
+        updateQuickEvents();
+    }
+
+    private void registerPSAListener() {
+        if (mRegistered) return;
+        mRegistered = true;
         IntentFilter psonalityIntent = new IntentFilter();
         psonalityIntent.addAction(Intent.ACTION_TIME_TICK);
         psonalityIntent.addAction(Intent.ACTION_TIME_CHANGED);
         psonalityIntent.addAction(Intent.ACTION_TIMEZONE_CHANGED);
         mContext.registerReceiver(mPSAListener, psonalityIntent);
-        updateQuickEvents();
+    }
+
+    private void unregisterPSAListener() {
+        if (!mRegistered) return;
+        mRegistered = false;
+        mContext.unregisterReceiver(mPSAListener);
     }
 
     public void updateQuickEvents() {
@@ -307,9 +320,11 @@ public class QuickEventsController {
 
     public void onPause() {
         mRunning = false;
+        unregisterPSAListener();
     }
 
     public void onResume() {
         mRunning = true;
+        registerPSAListener();
     }
 }
