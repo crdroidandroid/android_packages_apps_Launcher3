5a82aeae7a91cf51229dd6e5f73f4d62a98b1226
Ali B <abittin@gmail.com>
2021-01-30 22:36:40 +0300

    Launcher3: Add uninstall button to system shortcuts
    [ghostrider-reborn: Adapt to Sv2, 13]
    
    Squashed:
    
    Author: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Date:   Sun Jun 26 18:44:18 2022 +0530
    
        Launcher3: SystemShortcut: Avoid NPE due to uninstall button
    
        Crash log: https://bin.kv2.dev/~62b85c7008bb984d9dc61e94
    
        Steps to reproduce:
        - Add Now Playing widget to home screen
        - Long press on it
        - Launcher crashes
    
        Test: manual; no longer crashes
        Change-Id: I6c0f73e38a3695a64d820c40b58f5e87ba7013c5
    
    Change-Id: I005d676d9a98f65296c330e5e13fd0d849df6fe5
    Co-authored-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- quickstep/src/com/android/quickstep/TaskShortcutFactory.java
+++ quickstep/src/com/android/quickstep/TaskShortcutFactory.java
@@ -48,6 +48,7 @@ import com.android.launcher3.popup.SystemShortcut;
 import com.android.launcher3.popup.SystemShortcut.AppInfo;
 import com.android.launcher3.touch.PagedOrientationHandler;
 import com.android.launcher3.util.InstantAppResolver;
+import com.android.launcher3.util.PackageManagerHelper;
 import com.android.launcher3.util.SplitConfigurationOptions.SplitPositionOption;
 import com.android.quickstep.views.RecentsView;
 import com.android.quickstep.views.TaskThumbnailView;
@@ -324,6 +325,12 @@ public interface TaskShortcutFactory {
             return true;
         }
     };
+
+    TaskShortcutFactory UNINSTALL = (activity, taskContainer) ->
+            PackageManagerHelper.isSystemApp(activity,
+                 taskContainer.getTask().getTopComponent().getPackageName())
+                    ? null : new SystemShortcut.UnInstall(activity,
+                    taskContainer.getItemInfo(), taskContainer.getTaskView());
 
     TaskShortcutFactory FREE_FORM = new TaskShortcutFactory() {
         @Override
