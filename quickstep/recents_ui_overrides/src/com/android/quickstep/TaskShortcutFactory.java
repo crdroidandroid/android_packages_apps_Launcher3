b963f023929c1c03a7cca190c470ae24713c7dac
Ali B <abittin@gmail.com>
2021-01-30 22:36:40 +0300

    Launcher3: quickstep: add uninstall to task shortcuts
    Rewritten for Android 11 from
    	SystemShortcut: Add uninstall option
    	Author: Giuseppe Maggio <jertlok@protonmail.com>
    	ChangeID: I3ff34c1660ded0a4096b750deb0abce3f7b000c8
    
    Change-Id: I722c7ce33cf62bbb4482113fb999caad7e958059
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- quickstep/recents_ui_overrides/src/com/android/quickstep/TaskShortcutFactory.java
+++ quickstep/recents_ui_overrides/src/com/android/quickstep/TaskShortcutFactory.java
@@ -43,6 +43,7 @@ import com.android.launcher3.popup.SystemShortcut.AppInfo;
 import com.android.launcher3.userevent.nano.LauncherLogProto;
 import com.android.launcher3.util.Executors;
 import com.android.launcher3.util.InstantAppResolver;
+import com.android.launcher3.util.PackageManagerHelper;
 import com.android.quickstep.views.RecentsView;
 import com.android.quickstep.views.TaskThumbnailView;
 import com.android.quickstep.views.TaskView;
@@ -67,6 +68,11 @@ public interface TaskShortcutFactory {
     SystemShortcut getShortcut(BaseDraggingActivity activity, TaskView view);
 
     TaskShortcutFactory APP_INFO = (activity, view) -> new AppInfo(activity, view.getItemInfo());
+
+    TaskShortcutFactory UNINSTALL = (activity, view) ->
+            PackageManagerHelper.isSystemApp(activity,
+                 view.getTask().getTopComponent().getPackageName())
+                    ? null : new SystemShortcut.UnInstall(activity, view.getItemInfo());
 
     abstract class MultiWindowFactory implements TaskShortcutFactory {
 
