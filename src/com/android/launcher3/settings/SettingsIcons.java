98640e5c886a61c1692a231253fbe6d54caafedd
hundeva <hundeva@gmail.com>
2021-02-14 13:00:17 +0300

    Launcher3: Add option to change icon size
    * @Laker87 Adapt for 11
    
    bootleggers edit: Adapt to Launcher3
    
    *DU Edits*
    - Fix icons not applying
    
    *404 Edit*
    - Forward port to 10
    
    Change-Id: If14470f649dd75e91e273e873081c5640786f533
    Signed-off-by: Udit Karode <udit.karode@gmail.com>
    Signed-off-by: iamtoxyc <vtechnical1804@gmail.com>
    Signed-off-by: Laker87 <lakerman1987@gmail.com>

--- src/com/android/launcher3/settings/SettingsIcons.java
+++ src/com/android/launcher3/settings/SettingsIcons.java
@@ -32,7 +32,9 @@ import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentActivity;
 import androidx.fragment.app.FragmentManager;
+import androidx.preference.DropDownPreference;
 import androidx.preference.Preference;
+import androidx.preference.Preference.OnPreferenceChangeListener;
 import androidx.preference.PreferenceFragmentCompat;
 import androidx.preference.PreferenceFragmentCompat.OnPreferenceStartFragmentCallback;
 import androidx.preference.PreferenceFragmentCompat.OnPreferenceStartScreenCallback;
@@ -222,6 +224,18 @@ public class SettingsIcons extends FragmentActivity
                 case KEY_ICON_PACK:
                     setupIconPackPreference(preference);
                     return true;
+                    
+                case Utilities.ICON_SIZE:
+                    final DropDownPreference iconSizes = (DropDownPreference) findPreference(Utilities.ICON_SIZE);
+                    iconSizes.setSummary(iconSizes.getEntry());
+                    iconSizes.setOnPreferenceChangeListener(new OnPreferenceChangeListener() {
+                    public boolean onPreferenceChange(Preference preference, Object newValue) {
+                    int index = iconSizes.findIndexOfValue((String) newValue);
+                    iconSizes.setSummary(iconSizes.getEntries()[index]);
+                    Utilities.restart(getActivity());
+                    return true;
+                    }
+                });
             }
             return true;
         }
