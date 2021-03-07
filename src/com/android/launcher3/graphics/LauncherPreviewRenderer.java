c27fea5519d95a0fa13c4177c1b7e29b02e312e9
Anay Wadhera <awadhera@berkeley.edu>
2021-03-07 00:28:56 -0800

    Launcher: Use a custom icon factory to decide icon wrapping
    Change-Id: Ie287c918106e786cf6a2d77ca7188a2aa7556fa2
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/com/android/launcher3/graphics/LauncherPreviewRenderer.java
+++ src/com/android/launcher3/graphics/LauncherPreviewRenderer.java
@@ -65,9 +65,9 @@ import com.android.launcher3.R;
 import com.android.launcher3.WorkspaceLayoutManager;
 import com.android.launcher3.config.FeatureFlags;
 import com.android.launcher3.folder.FolderIcon;
-import com.android.launcher3.icons.BaseIconFactory;
 import com.android.launcher3.icons.BitmapInfo;
 import com.android.launcher3.icons.LauncherIcons;
+import com.android.launcher3.lineage.icon.LineageIconFactory;
 import com.android.launcher3.model.AllAppsList;
 import com.android.launcher3.model.BgDataModel;
 import com.android.launcher3.model.BgDataModel.Callbacks;
@@ -225,8 +225,8 @@ public class LauncherPreviewRenderer {
         mInsets.top = mInsets.bottom = (mDp.heightPx - mDp.availableHeightPx) / 2;
         mDp.updateInsets(mInsets);
 
-        BaseIconFactory iconFactory =
-                new BaseIconFactory(context, mIdp.fillResIconDpi, mIdp.iconBitmapSize) { };
+        LineageIconFactory iconFactory =
+                new LineageIconFactory(context, mIdp.fillResIconDpi, mIdp.iconBitmapSize) { };
         BitmapInfo iconInfo = iconFactory.createBadgedIconBitmap(new AdaptiveIconDrawable(
                         new ColorDrawable(Color.WHITE), new ColorDrawable(Color.WHITE)),
                 Process.myUserHandle(),
--- src/com/android/launcher3/icons/LauncherIcons.java
+++ src/com/android/launcher3/icons/LauncherIcons.java
@@ -21,12 +21,13 @@ import android.content.Context;
 import com.android.launcher3.InvariantDeviceProfile;
 import com.android.launcher3.graphics.IconShape;
 import com.android.launcher3.graphics.LauncherPreviewRenderer;
+import com.android.launcher3.lineage.icon.LineageIconFactory;
 
 /**
  * Wrapper class to provide access to {@link BaseIconFactory} and also to provide pool of this class
  * that are threadsafe.
  */
-public class LauncherIcons extends BaseIconFactory implements AutoCloseable {
+public class LauncherIcons extends LineageIconFactory implements AutoCloseable {
 
     private static final Object sPoolSync = new Object();
     private static LauncherIcons sPool;
