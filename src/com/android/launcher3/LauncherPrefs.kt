8edf841a9a78d30262190eda9f4abe59bb2a5603
Trishiraj <gamerprince.exp@gmail.com>
2021-09-23 03:34:14 +0530

    Launcher3: Add toggle for translucent hotseat background
    @neobuddy89: Disabled by default.
    
    Squashed below commits:
        From: Dil3mm4 <dil3mm4.dev@gmail.com>
        Date: Wed, 30 Dec 2020 20:38:39 +0000
        Subject: Appseat: add a bkg
    
        From: StarkDroid <gamerprince.exp@gmail.com>
        Date: Tue, 31 May 2022 16:16:56 -0500
        Subject: Hotseat background: Corner radius only at the top
    
        Rounding off at all sides makes it look like its not attached to anything
    
        From: minaripenguin <alexfinhart@gmail.com>
        Date: Fri, 22 Jul 2022 05:49:42 +0800
        Subject: Launcher3: Link hotseat bg corner radius to dialogCornerRadius attribute
    
    Change-Id: I7333e3fbe5e73188920f09c81304b3f6e8a5fc3e
    Signed-off-by: Trishiraj <gamerprince.exp@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/com/android/launcher3/LauncherPrefs.kt
+++ src/com/android/launcher3/LauncherPrefs.kt
@@ -249,6 +249,7 @@ constructor(@ApplicationContext private val encryptedContext: Context) {
         @JvmField val ROW_HEIGHT = backedUpItem("pref_row_height", 100)
         @JvmField val SHOW_DESKTOP_LABELS = backedUpItem("pref_desktop_show_labels", true)
         @JvmField val SHOW_DRAWER_LABELS = backedUpItem("pref_drawer_show_labels", true)
+        @JvmField val SHOW_HOTSEAT_BG = backedUpItem("pref_show_hotseat_bg", false)
         @JvmField val PROMISE_ICON_IDS = backedUpItem(InstallSessionHelper.PROMISE_ICON_IDS, "")
         @JvmField val WORK_EDU_STEP = backedUpItem("showed_work_profile_edu", 0)
         @JvmField val SLEEP_GESTURE = backedUpItem("pref_sleep_gesture", true)
