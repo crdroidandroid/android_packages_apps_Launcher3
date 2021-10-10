2712218e78bed680cd14a28cf75600c5c73fc90b
Danny Lin <danny@kdrag0n.dev>
2021-10-10 03:40:52 -0700

    Launcher3: Improve search bar UI in All Apps
    This is similar to, but not exactly the same as, Pixel stock:
    
    - Background color with contrast in light mode
    - Round, flat search bar surface with color matching the header
      background
    - Solid rounded background when positioned at the beginning of the list
    - Subtle placeholder text (medium weight) without icon
    - Same font size as Settings search bar
    
    Change-Id: Ifa9fabe3a8236513fad8030c0bd8ed4d27ebd549
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/com/android/launcher3/allapps/ActivityAllAppsContainerView.java
+++ src/com/android/launcher3/allapps/ActivityAllAppsContainerView.java
@@ -243,6 +243,7 @@ public class ActivityAllAppsContainerView<T extends Context & ActivityContext>
     @Override
     protected void updateHeaderScroll(int scrolledOffset) {
         super.updateHeaderScroll(scrolledOffset);
+        getSearchView().setBackgroundResource(R.drawable.bg_all_apps_searchbox);
         if (mSearchUiManager.getEditText() == null) {
             return;
         }
