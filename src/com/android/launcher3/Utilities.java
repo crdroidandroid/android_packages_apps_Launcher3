72c24b9780aedb02a05d55b27197a7f35b404ea4
Suphon Thanakornpakapong <paphonb@gmail.com>
2021-10-18 12:06:07 +0700

    Launcher3: Add Google Lens scan button to recents
    And a squash of:
    
    Launcher3: Restart launcher on changing google search/lens app
    
    idoybh (YAAP): Keep clear all rightmost
    
    Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>
    Co-authored-by: hundeva <hundeva@gmail.com>
    Change-Id: If00a4542ff7134ce4b84de3894f800d2b9a4f797
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/com/android/launcher3/Utilities.java
+++ src/com/android/launcher3/Utilities.java
@@ -165,6 +165,7 @@ public final class Utilities {
     public static final String GSA_PACKAGE = "com.google.android.googlequicksearchbox";
     public static final String LENS_ACTIVITY = "com.google.android.apps.lens.MainActivity";
     public static final String LENS_URI = "google://lens";
+    public static final String LENS_SHARE_ACTIVITY = "com.google.android.apps.search.lens.LensShareEntryPointActivity";
 
     public static final String KEY_DOCK_SEARCH = "pref_dock_search";
     public static final String KEY_DOCK_THEME = "pref_dock_theme";
