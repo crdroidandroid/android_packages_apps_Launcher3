6a00434e19691b4cd1f92c1f9cf043098ee24978
Saul Henriquez <saul_henriquez@hotmail.com>
2021-10-21 20:30:27 -0600

    Launcher3: Allow widgets to have 1 row as minimum size
    Signed-off-by: Saul Henriquez <saul_henriquez@hotmail.com>

--- src/com/android/launcher3/widget/LauncherAppWidgetProviderInfo.java
+++ src/com/android/launcher3/widget/LauncherAppWidgetProviderInfo.java
@@ -103,7 +103,7 @@ public class LauncherAppWidgetProviderInfo extends AppWidgetProviderInfo impleme
     public void initSpans(Context context, InvariantDeviceProfile idp) {
         mPM = context.getApplicationContext().getPackageManager();
         int minSpanX = 0;
-        int minSpanY = 0;
+        int minSpanY = 1;
         int maxSpanX = idp.numColumns;
         int maxSpanY = idp.numRows;
         int spanX = 0;
@@ -124,7 +124,7 @@ public class LauncherAppWidgetProviderInfo extends AppWidgetProviderInfo impleme
             minSpanX = Math.max(minSpanX,
                     getSpanX(widgetPadding, minResizeWidth, dp.cellLayoutBorderSpacePx.x,
                             cellSize.x));
-            minSpanY = Math.max(minSpanY,
+            minSpanY = Math.min(minSpanY,
                     getSpanY(widgetPadding, minResizeHeight, dp.cellLayoutBorderSpacePx.y,
                             cellSize.y));
 
