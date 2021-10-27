a0cbf9b0626636bc903891df068aae5e02056715
Patryk Michalik <contact@patrykmichalik.com>
2021-10-27 17:48:37 +0200

    Launcher3: Add ability to change corner radius of Dock search bar
    @neobuddy89: Rewrite and adapted for launcher3.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/com/android/launcher3/settings/SettingsHomescreen.java
+++ src/com/android/launcher3/settings/SettingsHomescreen.java
@@ -110,6 +110,7 @@ public class SettingsHomescreen extends CollapsingToolbarBaseActivity
         switch (key) {
             case Utilities.KEY_DOCK_SEARCH:
             case Utilities.KEY_DOCK_THEME:
+            case Utilities.KEY_SEARCH_RADIUS:
                 LauncherAppState.INSTANCE.executeIfCreated(app -> app.setNeedsRestart());
                 break;
             default:
