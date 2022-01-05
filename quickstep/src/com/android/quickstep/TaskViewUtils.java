417536b4f2e09aa321cf7cd8805805b7e051253b
wilsonshih <wilsonshih@google.com>
2022-01-05 16:59:50 +0800

    Fix NPE crash when merge transition to remote transition.
    After enable shell transition, the opening/closing target is for the
    app.
    In legacy RecentsAnimationController, the mode of the new
    RemoteAnimationTarget is decide based on the recents activity type, so
    basically the mode for all non-home/recents activity is closing.
    So while create the recents window animator, the target apps will be
    filter out in RemoteAnimationTargets#ctor because the mode is changed.
    To make the remote animation targets compatible with shell transition,
    there should only need to check the opening apps as target.
    
    Also fix that the topMostSimulators can be null if the size of
    remoteTargetHandles is zero.
    
    Bug: 207297486
    Test: Enable shell transition, run
    atest TaplTestsQuickstep#testQuickSwitchFromApp
    Test: Enable shell transition, enter recents, swipe to right-most page(
    the page shows "Clear all"), then click the task and verify Launcher
    won't crash. Test on both NexusLauncher and 3rd-party Launcher.
    
    Change-Id: I9bde3d7864d3edd54145c66acae0cd1013d89c6b
    Signed-off-by: strwbrry143 <alexfinhart@gmail.com>

--- quickstep/src/com/android/quickstep/TaskViewUtils.java
+++ quickstep/src/com/android/quickstep/TaskViewUtils.java
@@ -176,7 +176,7 @@ public final class TaskViewUtils {
                 ENABLE_QUICKSTEP_LIVE_TILE.get() && recentsView.getRunningTaskIndex() != -1;
         final RemoteAnimationTargets targets =
                 new RemoteAnimationTargets(appTargets, wallpaperTargets, nonAppTargets,
-                        inLiveTileMode ? MODE_CLOSING : MODE_OPENING);
+                        !ENABLE_SHELL_TRANSITIONS && inLiveTileMode ? MODE_CLOSING : MODE_OPENING);
         final RemoteAnimationTargetCompat navBarTarget = targets.getNavBarRemoteAnimationTarget();
 
         SurfaceTransactionApplier applier = new SurfaceTransactionApplier(v);
@@ -290,8 +290,8 @@ public final class TaskViewUtils {
             topMostSimulators = remoteTargetHandles;
         }
 
-        if (!skipViewChanges && parallaxCenterAndAdjacentTask &&
-                topMostSimulators != null && topMostSimulators.length > 0) {
+        if (!skipViewChanges && parallaxCenterAndAdjacentTask && topMostSimulators != null
+                && topMostSimulators.length > 0) {
             out.addFloat(v, VIEW_ALPHA, 1, 0, clampToProgress(LINEAR, 0.2f, 0.4f));
 
             RemoteTargetHandle[] simulatorCopies = topMostSimulators;
