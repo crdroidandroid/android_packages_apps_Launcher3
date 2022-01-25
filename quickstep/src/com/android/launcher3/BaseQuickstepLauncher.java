83a8e9cd0cdbba0d15407d45d4c56eb1544561ba
Jon Miranda <jonmiranda@google.com>
2022-01-24 18:00:04 -0800

    Remove ADAPTIVE_ICON_WINDOW_ANIM feature flag.
    Has been enabled for past few releases, no longer needed
    as a debug flag.
    
    Bug: 209856611
    Test: manual
    Change-Id: Id25e36522636e5c5d8ee60cdec49b3ee3f4d4251

--- quickstep/src/com/android/launcher3/BaseQuickstepLauncher.java
+++ quickstep/src/com/android/launcher3/BaseQuickstepLauncher.java
@@ -414,8 +414,7 @@ public abstract class BaseQuickstepLauncher extends Launcher
 
     @Override
     public boolean supportsAdaptiveIconAnimation(View clickedView) {
-        return mAppTransitionManager.hasControlRemoteAppTransitionPermission()
-                && FeatureFlags.ADAPTIVE_ICON_WINDOW_ANIM.get();
+        return mAppTransitionManager.hasControlRemoteAppTransitionPermission();
     }
 
     @Override
