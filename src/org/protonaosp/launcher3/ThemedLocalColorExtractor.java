11ea86530aa93f0c47de77905270556c71def92d
Anay Wadhera <anay1018@gmail.com>
2022-01-29 05:02:43 +0900

    Launcher: respect color/chroma override
    AEX Edits - Add custom_color check for overriding colors
    
    Change-Id: I227fa717fbce78583343654a3b98a8e505fb3b03
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/org/protonaosp/launcher3/ThemedLocalColorExtractor.java
+++ src/org/protonaosp/launcher3/ThemedLocalColorExtractor.java
@@ -16,12 +16,14 @@
 
 package org.protonaosp.launcher3;
 
+import android.annotation.ColorInt;
 import android.app.WallpaperColors;
 import android.app.WallpaperManager;
 import android.content.Context;
 import android.content.res.Resources;
 import android.graphics.Rect;
 import android.graphics.RectF;
+import android.provider.Settings;
 import android.util.SparseIntArray;
 import android.view.View;
 import android.widget.RemoteViews;
@@ -53,16 +55,7 @@ public class ThemedLocalColorExtractor extends LocalColorExtractor implements
     private static final SparseIntArray NEUTRAL1_RES = new SparseIntArray(13);
     private static final SparseIntArray NEUTRAL2_RES = new SparseIntArray(13);
 
-    // Viewing conditions and targets for theme generation
-    private final Zcam.ViewingConditions cond = new Zcam.ViewingConditions(
-            /* surroundFactor */ Zcam.ViewingConditions.SURROUND_AVERAGE,
-            /* adaptingLuminance */ 0.4 * CieXyzAbs.DEFAULT_SDR_WHITE_LUMINANCE,
-            /* backgroundLuminance */ new CieLab(50.0, 0.0, 0.0, Illuminants.D65)
-                    .toXyz().getY() * CieXyzAbs.DEFAULT_SDR_WHITE_LUMINANCE,
-            /* referenceWhite */ CieXyzAbs.fromRel(Illuminants.D65,
-                    CieXyzAbs.DEFAULT_SDR_WHITE_LUMINANCE)
-    );
-    private final ColorScheme targets = new MaterialYouTargets(1.0, false, cond);
+    private Context mContext;
 
     private final WallpaperManager wallpaperManager;
     private Listener listener;
@@ -145,6 +138,7 @@ public class ThemedLocalColorExtractor extends LocalColorExtractor implements
     }
 
     public ThemedLocalColorExtractor(Context context) {
+        mContext = context;
         wallpaperManager = (WallpaperManager) context.getSystemService(Context.WALLPAPER_SERVICE);
     }
 
@@ -178,8 +172,19 @@ public class ThemedLocalColorExtractor extends LocalColorExtractor implements
     @Override
     public SparseIntArray generateColorsOverride(WallpaperColors colors) {
         SparseIntArray colorRes = new SparseIntArray(5 * 13);
-        Color color = new Srgb(colors.getPrimaryColor().toArgb());
-        ColorScheme colorScheme = new DynamicColorScheme(targets, color, 1.0, cond, true);
+        double luminance = (double) Settings.Secure.getLong(mContext.getContentResolver(), "monet_engine_white_luminance_user", (long) CieXyzAbs.DEFAULT_SDR_WHITE_LUMINANCE);
+        Zcam.ViewingConditions cond = new Zcam.ViewingConditions(
+            /* surroundFactor */ Zcam.ViewingConditions.SURROUND_AVERAGE,
+            /* adaptingLuminance */ 0.4 * luminance,
+            /* backgroundLuminance */ new CieLab(50.0, 0.0, 0.0, Illuminants.D65)
+                    .toXyz().getY() * luminance,
+            /* referenceWhite */ CieXyzAbs.fromRel(Illuminants.D65, luminance)
+        );
+        ColorScheme targets = new MaterialYouTargets(getChroma(), false, cond);
+        @ColorInt int customColor = Settings.Secure.getInt(mContext.getContentResolver(), "monet_engine_custom_color", 0);
+        @ColorInt int colorOverride = Settings.Secure.getInt(mContext.getContentResolver(), "monet_engine_color_override", -1);
+        Color color = new Srgb((colorOverride != -1 && customColor != 0)  ? colorOverride : colors.getPrimaryColor().toArgb());
+        ColorScheme colorScheme = new DynamicColorScheme(targets, color, getChroma(), cond, true);
 
         addColorsToArray(colorScheme.getAccent1(), ACCENT1_RES, colorRes);
         addColorsToArray(colorScheme.getAccent2(), ACCENT2_RES, colorRes);
@@ -261,5 +266,9 @@ public class ThemedLocalColorExtractor extends LocalColorExtractor implements
         if (listener != null) {
             listener.onColorsChanged(area, generateColorsOverride(colors));
         }
+    }
+
+    private float getChroma() {
+        return Settings.Secure.getFloat(mContext.getContentResolver(), "monet_engine_chroma_factor", 1.0f);
     }
 }
