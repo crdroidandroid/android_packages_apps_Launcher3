94e8aacdb8a7e2e3da3b455fae95ebc41f7d1354
Suphon Thanakornpakapong <paphonb@gmail.com>
2022-02-02 17:03:39 +0530

    Launcher3: Implement Hotseat searchbar
    [Maitreya29: Rewrite QsbLayout and AssistantIconView in java and dynamically color the searchbar]
    
    @neobuddy89: Updated for Android 12L
    
    Co-authored-by: Maitreya29 <maitreyapatni30@gmail.com>
    Change-Id: I885e21ec8d7b8a6377cb228ff0099b0decfade02
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/com/android/launcher3/DeviceProfile.java
+++ src/com/android/launcher3/DeviceProfile.java
@@ -338,7 +338,7 @@ public class DeviceProfile {
                 isTwoPanels ? inv.numDatabaseAllAppsColumns : inv.numAllAppsColumns;
         hotseatBarSizeExtraSpacePx = 0;
         hotseatBarTopPaddingPx =
-                res.getDimensionPixelSize(R.dimen.dynamic_grid_hotseat_top_padding);
+                res.getDimensionPixelSize(R.dimen.dynamic_grid_hotseat_bottom_non_tall_padding);
         hotseatBarBottomPaddingPx = (isTallDevice ? 0
                 : res.getDimensionPixelSize(R.dimen.dynamic_grid_hotseat_bottom_non_tall_padding))
                 + res.getDimensionPixelSize(R.dimen.dynamic_grid_hotseat_bottom_padding);
