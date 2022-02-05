d9f9c5dd4af51d5d54e0686c07b581f00096d9e7
LibXZR <i@xzr.moe>
2022-02-05 16:33:05 +0800

    Launcher3: Introduce overview scrolling scale animation
    This vivid animation enables users to perceive more clearly
    about the switching of the task cards.
    
    Change-Id: Ie1350d6bda2d311f70f9be7cff47b0abe7e00cad
    Signed-off-by: LibXZR <i@xzr.moe>
    
    Fix discontinuous animation when dismissing taskcards on
     the right side
    
    Cards should never be scale down in this animation.
    
    Change-Id: I759492390eb8f1a2b9023c2d264f0e7191b17108
    Signed-off-by: LibXZR <i@xzr.moe>
    
    Recalculate scroll scale on layout
    
    Edge case exist that when there's only one task,
    onScrollChanged() won't be called so the last scale
    from the recycled view is showed, leading to wrong
    display size.
    
    Change-Id: If8a189ba6b854ed486283d43b8a0ac3e4bb4e9f2
    Signed-off-by: LibXZR <i@xzr.moe>
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- quickstep/src/com/android/quickstep/views/RecentsView.java
+++ quickstep/src/com/android/quickstep/views/RecentsView.java
@@ -18,12 +18,14 @@ package com.android.quickstep.views;
 
 import static android.app.ActivityTaskManager.INVALID_TASK_ID;
 import static android.view.Surface.ROTATION_0;
+import static android.view.Surface.ROTATION_180;
 import static android.view.View.MeasureSpec.EXACTLY;
 import static android.view.View.MeasureSpec.makeMeasureSpec;
 
 import static com.android.launcher3.AbstractFloatingView.TYPE_TASK_MENU;
 import static com.android.launcher3.AbstractFloatingView.getTopOpenViewWithType;
 import static com.android.launcher3.BaseActivity.STATE_HANDLER_INVISIBILITY_FLAGS;
+import static com.android.launcher3.LauncherAnimUtils.SCALE_PROPERTY;
 import static com.android.launcher3.LauncherAnimUtils.SUCCESS_TRANSITION_PROGRESS;
 import static com.android.launcher3.LauncherAnimUtils.VIEW_ALPHA;
 import static com.android.launcher3.LauncherState.BACKGROUND_APP;
@@ -534,6 +536,8 @@ public abstract class RecentsView<ACTIVITY_TYPE extends StatefulActivity<STATE_T
     private int mOverScrollShift = 0;
     private long mScrollLastHapticTimestamp;
 
+    private float mScrollScale = 1f;
+
     /**
      * TODO: Call reloadIdNeeded in onTaskStackChanged.
      */
@@ -810,6 +814,8 @@ public abstract class RecentsView<ACTIVITY_TYPE extends StatefulActivity<STATE_T
         }
         // make sure filter is turned off by default
         mFilterState.setFilterBy(null);
+
+        mScrollScale = getResources().getFloat(R.dimen.overview_scroll_scale);
     }
 
     /** Get the state of the filter */
@@ -3453,6 +3459,15 @@ public abstract class RecentsView<ACTIVITY_TYPE extends StatefulActivity<STATE_T
                     }
                 }
 
+                if (i == dismissedIndex + 1 ||
+                        dismissedIndex == taskCount -1 && i == dismissedIndex - 1) {
+                    if (child.getScaleX() <= dismissedTaskView.getScaleX())
+                        anim.setFloat(child, SCALE_PROPERTY,
+                            dismissedTaskView.getScaleX(), LINEAR);
+                    else
+                        anim.setFloat(child, SCALE_PROPERTY, 1f, LINEAR);
+                }
+
                 int scrollDiff = newScroll[i] - oldScroll[i] + offset;
                 if (scrollDiff != 0) {
                     FloatProperty translationProperty = child instanceof TaskView
@@ -4201,6 +4216,7 @@ public abstract class RecentsView<ACTIVITY_TYPE extends StatefulActivity<STATE_T
                         .setScroll(getScrollOffset()));
         setImportantForAccessibility(isModal() ? IMPORTANT_FOR_ACCESSIBILITY_NO
                 : IMPORTANT_FOR_ACCESSIBILITY_AUTO);
+        doScrollScale();
     }
 
     private void updatePivots() {
@@ -5799,6 +5815,32 @@ public abstract class RecentsView<ACTIVITY_TYPE extends StatefulActivity<STATE_T
     protected void onScrollChanged(int l, int t, int oldl, int oldt) {
         super.onScrollChanged(l, t, oldl, oldt);
         dispatchScrollChanged();
+        doScrollScale();
+    }
+
+    private void doScrollScale() {
+        if (showAsGrid())
+            return;
+
+        boolean isInLandscape = mOrientationState.getTouchRotation() != ROTATION_0
+                                && mOrientationState.getTouchRotation() != ROTATION_180;
+        int childCount = Math.min(mPageScrolls.length, getChildCount());
+        int curScroll = isInLandscape ? getScrollY() : getScrollX();
+
+        for (int i = 0; i < childCount; i++) {
+            View child = getChildAt(i);
+            int scaleArea = child.getWidth() + mPageSpacing;
+            int childPosition = mPageScrolls[i];
+            int scrollDelta = Math.abs(curScroll - childPosition);
+            if (scrollDelta > scaleArea) {
+                child.setScaleX(mScrollScale);
+                child.setScaleY(mScrollScale);
+            } else {
+                float scale = mapToRange(scrollDelta, 0, scaleArea, 1f, mScrollScale, LINEAR);
+                child.setScaleX(scale);
+                child.setScaleY(scale);
+            }
+        }
     }
 
     private void dispatchScrollChanged() {
