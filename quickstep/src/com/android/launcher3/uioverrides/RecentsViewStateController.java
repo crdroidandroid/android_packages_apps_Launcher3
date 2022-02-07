0755a0d60288a3e0eb1528145be98cd497a3d54d
LibXZR <i@xzr.moe>
2022-02-07 10:59:50 +0800

    Launcher3: Introduce memory info in overview
    This is convenient for monitoring device status and many OEMs have their design for this. It's time to have our own one.
    
    [PacMM79: Adapt to crdroid launcher. Update quickstep overview actions dimens.xml]
    
    Change-Id: I890c86c798055b1964506d9a4df1cef386562160
    Signed-off-by: LibXZR <i@xzr.moe>
    iusmac: adapt to A13.
    Signed-off-by: iusmac <iusico.maxim@libero.it>
    neobuddy89: adapt to A13 QPR2
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- quickstep/src/com/android/launcher3/uioverrides/RecentsViewStateController.java
+++ quickstep/src/com/android/launcher3/uioverrides/RecentsViewStateController.java
@@ -16,6 +16,7 @@
 package com.android.launcher3.uioverrides;
 
 import static com.android.launcher3.LauncherState.CLEAR_ALL_BUTTON;
+import static com.android.launcher3.LauncherState.MEMINFO;
 import static com.android.launcher3.LauncherState.OVERVIEW_ACTIONS;
 import static com.android.launcher3.LauncherState.OVERVIEW_SPLIT_SELECT;
 import static com.android.launcher3.anim.Interpolators.LINEAR;
@@ -48,6 +49,7 @@ import com.android.quickstep.util.AnimUtils;
 import com.android.quickstep.util.SplitAnimationTimings;
 import com.android.quickstep.views.ClearAllButton;
 import com.android.quickstep.views.LauncherRecentsView;
+import com.android.quickstep.views.MemInfoView;
 import com.android.quickstep.views.RecentsView;
 
 /**
@@ -163,6 +165,9 @@ public final class RecentsViewStateController extends
         propertySetter.setFloat(mLauncher.getActionsView().getVisibilityAlpha(),
                 MULTI_PROPERTY_VALUE, overviewButtonAlpha, config.getInterpolator(
                         ANIM_OVERVIEW_ACTIONS_FADE, LINEAR));
+        float memInfoAlpha = state.areElementsVisible(mLauncher, MEMINFO) ? 1 : 0;
+        propertySetter.setFloat(mLauncher.getMemInfoView(), MemInfoView.STATE_CTRL_ALPHA,
+                memInfoAlpha, LINEAR);
     }
 
     @Override
