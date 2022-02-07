3a5bdf11cc711163a879c852725b8714cdb4b3ef
Pranav Vashi <neobuddy89@gmail.com>
2022-02-07 19:12:36 +0530

    Launcher3: Add toggle to theme dock searchbar icons
    Squashed:
    
        From: Adithya R <gh0strider.2k18.reborn@gmail.com>
        Date: Wed, 1 Nov 2023 17:53:36 +0530
        Subject: Launcher3: Update QSB lens icon (2023 edition)
    
        From husky U1B2. Matches the latest Google search widget.
    
        Change-Id: I02038530581f7d2900b1c069562fc0360b0e4b50
        Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/com/android/launcher3/settings/SettingsHomescreen.java
+++ src/com/android/launcher3/settings/SettingsHomescreen.java
@@ -115,7 +115,8 @@ public class SettingsHomescreen extends CollapsingToolbarBaseActivity
     public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
         if (LauncherPrefs.SHOW_HOTSEAT_BG.getSharedPrefKey().equals(key) ||
                 LauncherPrefs.HOTSEAT_OPACITY.getSharedPrefKey().equals(key) ||
-                LauncherPrefs.DOCK_SEARCH.getSharedPrefKey().equals(key)) {
+                LauncherPrefs.DOCK_SEARCH.getSharedPrefKey().equals(key) ||
+                LauncherPrefs.DOCK_THEME.getSharedPrefKey().equals(key)) {
             LauncherAppState.INSTANCE.executeIfCreated(app -> app.setNeedsRestart());
         }
     }
