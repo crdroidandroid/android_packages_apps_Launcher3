74789611224cb40524577d111a5b2bc02efb068c
Pranav Vashi <neobuddy89@gmail.com>
2022-02-07 19:12:36 +0530

    Launcher3: Add toggle to theme dock searchbar icons
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/com/android/launcher3/Utilities.java
+++ src/com/android/launcher3/Utilities.java
@@ -160,6 +160,7 @@ public final class Utilities {
     public static final String LENS_URI = "google://lens";
 
     public static final String KEY_DOCK_SEARCH = "pref_dock_search";
+    public static final String KEY_DOCK_THEME = "pref_dock_theme";
 
     /**
      * Returns true if theme is dark.
@@ -973,5 +974,10 @@ public final class Utilities {
     private static boolean isQSBEnabled(Context context) {
         SharedPreferences prefs = getPrefs(context.getApplicationContext());
         return prefs.getBoolean(KEY_DOCK_SEARCH, true);
+    }
+
+    public static boolean isThemedIconsEnabled(Context context) {
+        SharedPreferences prefs = getPrefs(context.getApplicationContext());
+        return prefs.getBoolean(KEY_DOCK_THEME, false);
     }
 }
