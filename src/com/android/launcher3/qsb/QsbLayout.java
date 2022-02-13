a88c68df80169105e2fb6321ae782cf83a348092
Pranav Vashi <neobuddy89@gmail.com>
2022-02-13 23:16:31 +0530

    Launcher3: Make google icon in dock searchbar clickable
    * Also reduce widget height to match actual one.
    
    Squashed:
    
        From: Pranav Vashi <neobuddy89@gmail.com>
        Date: Tue, 18 Oct 2022 00:53:05 +0530
        Subject: [PATCH 137/331] Launcher3: Fix crash launching google app from dock
         search bar
    
        * Seems google search update is messed up. This commit fixes crash.
    
        Log:
        time: 1665954887261
        msg: android.content.ActivityNotFoundException: Unable to find explicit activity class {com.google.android.googlequicksearchbox/com.google.android.googlequicksearchbox.MainInfoGatewayInternal}; have you declared this activity in your AndroidManifest.xml, or does your intent not match its declared <intent-filter>?
        stacktrace: android.content.ActivityNotFoundException: Unable to find explicit activity class {com.google.android.googlequicksearchbox/com.google.android.googlequicksearchbox.MainInfoGatewayInternal}; have you declared this activity in your AndroidManifest.xml, or does your intent not match its declared <intent-filter>?
    	    at android.app.Instrumentation.checkStartActivityResult(Instrumentation.java:2164)
    	    at android.app.Instrumentation.execStartActivity(Instrumentation.java:1811)
    	    at android.app.Activity.startActivityForResult(Activity.java:5470)
    	    at com.android.launcher3.Launcher.startActivityForResult(Launcher.java:1756)
    	    at com.android.launcher3.BaseQuickstepLauncher.startActivityForResult(BaseQuickstepLauncher.java:304)
    	    at android.app.Activity.startActivityForResult(Activity.java:5428)
    	    at android.app.Activity.startActivity(Activity.java:5926)
    	    at android.app.Activity.startActivity(Activity.java:5893)
    	    at android.content.ContextWrapper.startActivity(ContextWrapper.java:432)
    	    at com.android.launcher3.qsb.QsbLayout.lambda$setupGIcon$1$com-android-launcher3-qsb-QsbLayout(QsbLayout.java:123)
    	    at com.android.launcher3.qsb.QsbLayout$$ExternalSyntheticLambda2.onClick(Unknown Source:4)
    	    at android.view.View.performClick(View.java:7506)
    	    at android.view.View.performClickInternal(View.java:7483)
    	    at android.view.View.-$$Nest$mperformClickInternal(Unknown Source:0)
    	    at android.view.View$PerformClick.run(View.java:29335)
    	    at android.os.Handler.handleCallback(Handler.java:942)
    	    at android.os.Handler.dispatchMessage(Handler.java:99)
    	    at android.os.Looper.loopOnce(Looper.java:201)
    	    at android.os.Looper.loop(Looper.java:288)
    	    at android.app.ActivityThread.main(ActivityThread.java:7898)
    	    at java.lang.reflect.Method.invoke(Native Method)
    	    at com.android.internal.os.RuntimeInit$MethodAndArgsCaller.run(RuntimeInit.java:548)
    	    at com.android.internal.os.ZygoteInit.main(ZygoteInit.java:936)
    
        Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    
        From: Pranav Vashi <neobuddy89@gmail.com>
        Date: Wed, 27 Dec 2023 22:42:30 +0530
        Subject: Launcher3: Prevent hotseat QSB crash with google stub
    
        Fixes: https://github.com/crdroidandroid/issue_tracker/issues/286
        Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/com/android/launcher3/qsb/QsbLayout.java
+++ src/com/android/launcher3/qsb/QsbLayout.java
@@ -62,6 +62,7 @@ public class QsbLayout extends FrameLayout {
             gIcon.setImageResource(R.drawable.ic_super_g_color);
             lensIcon.setImageResource(R.drawable.ic_lens_color);
         }
+        setupGIcon();
         setupLensIcon();
     }
 
@@ -73,6 +74,8 @@ public class QsbLayout extends FrameLayout {
         micIcon.setBackground(pd);
         lensIcon.setClipToOutline(cornerRadius > 0);
         lensIcon.setBackground(pd);
+        gIcon.setClipToOutline(cornerRadius > 0);
+        gIcon.setBackground(pd);
     }
 
     private void setUpBackground() {
@@ -112,6 +115,18 @@ public class QsbLayout extends FrameLayout {
             mContext.startActivity(new Intent("android.search.action.GLOBAL_SEARCH").addFlags(Intent.FLAG_ACTIVITY_NEW_TASK |
                 Intent.FLAG_ACTIVITY_CLEAR_TASK).setPackage(searchPackage));
         });
+    }
+
+    private void setupGIcon() {
+        try {
+            Intent intent = mContext.getPackageManager().getLaunchIntentForPackage(Utilities.GSA_PACKAGE);
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            gIcon.setOnClickListener(view -> {
+                mContext.startActivity(intent);
+            });
+        } catch (Exception e) {
+            // Do nothing
+        }
     }
 
     private void setupLensIcon() {
