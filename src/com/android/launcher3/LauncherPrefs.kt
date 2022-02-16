bb75771848c7df642d26e8a356edc4dd40e9acae
Pranav Vashi <neobuddy89@gmail.com>
2022-02-16 20:48:05 +0530

    Launcher3: Add toggle to hide status bar
    Ref: https://github.com/LawnchairLauncher/lawnchair/commit/0a3447fda111eed71628c2eb7c3aa6a8f04efa0d
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/com/android/launcher3/LauncherPrefs.kt
+++ src/com/android/launcher3/LauncherPrefs.kt
@@ -259,6 +259,7 @@ constructor(@ApplicationContext private val encryptedContext: Context) {
         @JvmField val SHOW_DESKTOP_LABELS = backedUpItem("pref_desktop_show_labels", true)
         @JvmField val SHOW_DRAWER_LABELS = backedUpItem("pref_drawer_show_labels", true)
         @JvmField val SHOW_HOTSEAT_BG = backedUpItem("pref_show_hotseat_bg", false)
+        @JvmField val SHOW_STATUS_BAR = backedUpItem("pref_show_statusbar", true)
         @JvmField val PROMISE_ICON_IDS = backedUpItem(InstallSessionHelper.PROMISE_ICON_IDS, "")
         @JvmField val WALLPAPER_SCROLLING = backedUpItem("pref_allow_wallpaper_scrolling", true)
         @JvmField val WORK_EDU_STEP = backedUpItem("showed_work_profile_edu", 0)
