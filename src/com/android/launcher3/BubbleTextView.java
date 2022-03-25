836540c33b424f693e77e8b1e43692ff7ec9c6e9
Harsh <harsh@project404.us>
2022-03-26 02:24:55 +0530

    Launcher3: Apply themed icons in all apps drawer
    Change-Id: I013d4b1e3280f44623d2c11f0f61d693f3c0ea27
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/com/android/launcher3/BubbleTextView.java
+++ src/com/android/launcher3/BubbleTextView.java
@@ -411,7 +411,7 @@ public class BubbleTextView extends TextView implements ItemInfoUpdateReceiver,
 
     protected boolean shouldUseTheme() {
         return mDisplay == DISPLAY_WORKSPACE || mDisplay == DISPLAY_FOLDER
-                || mDisplay == DISPLAY_TASKBAR;
+                || mDisplay == DISPLAY_TASKBAR || mDisplay == DISPLAY_ALL_APPS;
     }
 
     @UiThread
