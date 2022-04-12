51ffd0167e1c1fa51db0809082101c218d2eac7c
Pranav <npv12@iitbbs.ac.in>
2022-04-12 10:40:56 +0530

    Launcher3: return if recentsView is null
    Change-Id: I91787882991df06ed6b3c71a36b106917855d773
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- quickstep/src/com/android/quickstep/TaskViewUtils.java
+++ quickstep/src/com/android/quickstep/TaskViewUtils.java
@@ -168,6 +168,10 @@ public final class TaskViewUtils {
         boolean isQuickSwitch = v.isEndQuickswitchCuj();
         v.setEndQuickswitchCuj(false);
 
+        if (recentsView == null) {
+            return;
+        }
+
         final RemoteAnimationTargets targets =
                 new RemoteAnimationTargets(appTargets, wallpaperTargets, nonAppTargets,
                         MODE_OPENING);
