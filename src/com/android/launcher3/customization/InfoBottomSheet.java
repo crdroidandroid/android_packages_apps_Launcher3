ef2eca7888a6e7a555cdff7195849e184fe356d6
Pranav Vashi <neobuddy89@gmail.com>
2022-04-24 01:51:09 +0530

    Launcher3: Fix NPE with AppInfoBottomSheet
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/com/android/launcher3/customization/InfoBottomSheet.java
+++ src/com/android/launcher3/customization/InfoBottomSheet.java
@@ -148,4 +148,9 @@ public class InfoBottomSheet extends WidgetsBottomSheet {
             return false;
         }
     }
+
+    @Override
+    protected boolean hasSeenEducationTip() {
+        return true;
+    }
 }
