e63c657741de2ddb4505aef0de3764c715bc7fd7
Pranav Vashi <neobuddy89@gmail.com>
2022-05-02 02:38:15 +0530

    Launcher3: Make taskbar untouchable during transitions
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- quickstep/src/com/android/launcher3/taskbar/TaskbarLauncherStateController.java
+++ quickstep/src/com/android/launcher3/taskbar/TaskbarLauncherStateController.java
@@ -156,7 +156,7 @@ import java.util.function.Supplier;
     }
 
     public boolean isAnimatingToLauncher() {
-        return mIsAnimatingToLauncherViaResume || mIsAnimatingToLauncherViaGesture;
+        return mIsAnimatingToLauncherViaResume || mIsAnimatingToLauncherViaGesture || hasAnyFlag(FLAG_TRANSITION_STATE_RUNNING);
     }
 
     /**
