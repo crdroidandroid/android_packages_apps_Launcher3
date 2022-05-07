496b69fce2db4d17b2b7763a549215f0d8068a21
Adithya R <gh0strider.2k18.reborn@gmail.com>
2022-05-07 18:26:16 +0530

    Launcher3: Allow hiding top shadow on statusbar
    On certain devices (especially amoled) with certain
    black-themed wallpapers, it looks quite bad. Add a
    toggle to disable it, inspired from lawnchair:
    
    https://github.com/LawnchairLauncher/lawnchair/commit/c99e61205cc4d5b5d3d9b827787c9fd873616060
    https://github.com/LawnchairLauncher/lawnchair/commit/e41e27a4d11e7f8352a3d2fa74afb04e8af9354a
    
    Squashed:
    
        From: Adithya R <gh0strider.2k18.reborn@gmail.com>
        Date: Sun, 10 Nov 2024 22:54:28 +0530
        Subject: fixup! Launcher3: Allow hiding top shadow on statusbar
    
        Change-Id: I962faf5586eb9493d1bf69bcdf4652f4c62f9d1e
        Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    
    Co-authored-by: Suphon Thanakornpakapong <paphonb@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/com/android/launcher3/LauncherPrefs.kt
+++ src/com/android/launcher3/LauncherPrefs.kt
@@ -259,6 +259,7 @@ constructor(@ApplicationContext private val encryptedContext: Context) {
         @JvmField val SHOW_DRAWER_LABELS = backedUpItem("pref_drawer_show_labels", true)
         @JvmField val SHOW_HOTSEAT_BG = backedUpItem("pref_show_hotseat_bg", false)
         @JvmField val SHOW_STATUS_BAR = backedUpItem("pref_show_statusbar", true)
+        @JvmField val SHOW_TOP_SHADOW = backedUpItem("pref_show_top_shadow", true)
         @JvmField val SLEEP_GESTURE = backedUpItem("pref_sleep_gesture", true)
         @JvmField val SLEEP_GESTURE_HAPTIC = backedUpItem("pref_sleep_gesture_haptic", true)
         @JvmField val PROMISE_ICON_IDS = nonRestorableItem(InstallSessionHelper.PROMISE_ICON_IDS, "")
