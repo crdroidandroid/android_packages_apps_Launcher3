64bd06b36bab5ff354e822b185fc1a192fe51b81
Pranav Vashi <neobuddy89@gmail.com>
2022-06-05 13:14:15 +0530

    Launcher3: Make drawer cells square when labels are hidden
    Ref: https://github.com/LawnchairLauncher/lawnchair/commit/96fa31a774b55f323fecc7d0bcf3131334f65369
    
    Adapted and updated for A13
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/com/android/launcher3/DeviceProfile.java
+++ src/com/android/launcher3/DeviceProfile.java
@@ -224,6 +224,7 @@ public class DeviceProfile {
     public final int numShownAllAppsColumns;
     public float allAppsIconTextSizePx;
     private float allAppsCellHeightMultiplier;
+    private boolean allAppsIconText;
 
     // Overview
     public int overviewTaskMarginPx;
@@ -362,6 +363,7 @@ public class DeviceProfile {
 
         allAppsCellHeightMultiplier =
                     (float) prefs.getInt(KEY_ROW_HEIGHT, 100) / 100F;
+        allAppsIconText = prefs.getBoolean(InvariantDeviceProfile.KEY_SHOW_DRAWER_LABELS, true);
 
         edgeMarginPx = res.getDimensionPixelSize(R.dimen.dynamic_grid_edge_margin);
         workspaceContentScale = res.getFloat(R.dimen.workspace_content_scale);
@@ -832,6 +834,14 @@ public class DeviceProfile {
         int baseCellHeight = allAppsIconSizePx + allAppsIconDrawablePaddingPx
                 + textHeight + (topBottomPadding * 2);
         allAppsCellHeightPx = (int) (baseCellHeight * allAppsCellHeightMultiplier);
+        if (!allAppsIconText) {
+            int cellLayoutHorizontalPadding =
+                    (cellLayoutPaddingPx.left + cellLayoutPaddingPx.right) / 2;
+            int leftRightPadding = desiredWorkspaceHorizontalMarginPx + cellLayoutHorizontalPadding;
+            int drawerWidth = availableWidthPx - leftRightPadding * 2;
+            allAppsCellHeightPx = (int) (drawerWidth / inv.numAllAppsColumns * allAppsCellHeightMultiplier);
+            allAppsIconDrawablePaddingPx = 0;
+        }
     }
 
     private void updateAllAppsContainerWidth(Resources res) {
@@ -1078,6 +1088,15 @@ public class DeviceProfile {
             allAppsIconDrawablePaddingPx =
                     res.getDimensionPixelSize(R.dimen.all_apps_icon_drawable_padding);
             allAppsCellWidthPx = allAppsIconSizePx + (2 * allAppsIconDrawablePaddingPx);
+        }
+
+        if (!allAppsIconText) {
+            int cellLayoutHorizontalPadding =
+                    (cellLayoutPaddingPx.left + cellLayoutPaddingPx.right) / 2;
+            int leftRightPadding = desiredWorkspaceHorizontalMarginPx + cellLayoutHorizontalPadding;
+            int drawerWidth = availableWidthPx - leftRightPadding * 2;
+            allAppsCellHeightPx = (int) (drawerWidth / inv.numAllAppsColumns * allAppsCellHeightMultiplier);
+            allAppsIconDrawablePaddingPx = 0;
         }
 
         updateAllAppsContainerWidth(res);
