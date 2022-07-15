f18488946a649b38ce3b3e4e03ee0393a9ac2bc4
Pranav Vashi <neobuddy89@gmail.com>
2022-07-15 17:47:04 +0530

    Launcher3: Add recents and app drawer opacity customization
    Squashed:
    
        From: nift4 <nift4@protonmail.com>
        Date: Mon, 7 Aug 2023 19:47:20 +0200
        Subject: Launcher3: Respect transparent app drawer in tablet UI too
    
        excluding taskbar is a design decision as it might look weird/unreadable
        over recents or random app content
    
        Change-Id: I2d0e24b4340e7bea923b15dff73481c4088223c9
        Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    
        From: Pranav Vashi <neobuddy89@gmail.com>
        Date: Sun, 9 Apr 2023 16:59:03 +0530
        Subject: Launcher3: Follow all apps background for taskbar slide in view
    
        From: Pranav Vashi <neobuddy89@gmail.com>
        Date: Sat, 27 Jul 2024 11:35:56 +0530
        Subject: Launcher3: Fix apps drawer background on tablets for light theme
    
        * widgets_picker_scrim produces dark color on light theme.
        * There are still usages of widgets_picker_scrim left but
          no other reports of issue.
    
        Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- quickstep/src/com/android/launcher3/uioverrides/states/OverviewState.java
+++ quickstep/src/com/android/launcher3/uioverrides/states/OverviewState.java
@@ -25,10 +25,12 @@ import android.content.Context;
 import android.graphics.Rect;
 import android.os.SystemProperties;
 
+import androidx.core.graphics.ColorUtils;
+
 import com.android.launcher3.DeviceProfile;
 import com.android.launcher3.Launcher;
-import com.android.launcher3.LauncherState;
 import com.android.launcher3.LauncherPrefs;
+import com.android.launcher3.LauncherState;
 import com.android.launcher3.R;
 import com.android.launcher3.util.DisplayController;
 import com.android.launcher3.util.Themes;
@@ -167,9 +169,9 @@ public class OverviewState extends LauncherState {
 
     @Override
     public int getWorkspaceScrimColor(Launcher launcher) {
-        return enableOverviewBackgroundWallpaperBlur() && BlurUtils.supportsBlursOnWindows()
-                ? Themes.getAttrColor(launcher, R.attr.overviewScrimColorOverBlur)
-                : Themes.getAttrColor(launcher, R.attr.overviewScrimColor);
+        return ColorUtils.setAlphaComponent(
+                Themes.getAttrColor(launcher, R.attr.overviewScrimColor),
+                LauncherPrefs.RECENTS_OPACITY.get(launcher) * 255 / 100);
     }
 
     @Override
