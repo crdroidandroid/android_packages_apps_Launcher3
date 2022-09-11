42ed6386d18cc0d8b74c259b35a4e529477972b5
str-br <alexfinhart@gmail.com>
2022-09-11 21:40:57 +0800

    Launcher3: Enable One all apps search feature
    Signed-off-by: Dmitrii <bankersenator@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/com/android/launcher3/config/FeatureFlags.java
+++ src/com/android/launcher3/config/FeatureFlags.java
@@ -235,7 +235,7 @@ public final class FeatureFlags {
             "Enables displaying the all apps button in the hotseat.");
 
     public static final BooleanFlag ENABLE_ALL_APPS_ONE_SEARCH_IN_TASKBAR = getDebugFlag(
-            "ENABLE_ALL_APPS_ONE_SEARCH_IN_TASKBAR", false,
+            "ENABLE_ALL_APPS_ONE_SEARCH_IN_TASKBAR", true,
             "Enables One Search box in Taskbar All Apps.");
 
     public static final BooleanFlag ENABLE_SPLIT_FROM_WORKSPACE = getDebugFlag(
