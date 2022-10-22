8237224dd5871a5de6488e5db64c44b9079772bb
Pranav Vashi <neobuddy89@gmail.com>
2022-10-22 23:45:49 +0530

    Launcher3: Fix error in TaskbarManager for registering shared prefs
    * Registed after user is unlocked. Tested and works fine in all use cases.
    * Fixes:
        java.lang.IllegalStateException: SharedPreferences in credential encrypted storage are not available until after user is unlocked
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- quickstep/src/com/android/launcher3/taskbar/TaskbarManager.java
+++ quickstep/src/com/android/launcher3/taskbar/TaskbarManager.java
@@ -149,9 +149,6 @@ public class TaskbarManager implements OnSharedPreferenceChangeListener {
         mUserSetupCompleteListener = isUserSetupComplete -> recreateTaskbar();
         mNavBarKidsModeListener = isNavBarKidsMode -> recreateTaskbar();
 
-        SharedPreferences prefs = LauncherPrefs.getPrefs(mContext);
-        prefs.registerOnSharedPreferenceChangeListener(this);
-
         // TODO(b/227669780): Consolidate this w/ DisplayController callbacks
         mComponentCallbacks = new ComponentCallbacks() {
             private Configuration mOldConfig = mContext.getResources().getConfiguration();
@@ -298,6 +295,8 @@ public class TaskbarManager implements OnSharedPreferenceChangeListener {
      */
     public void onUserUnlocked() {
         mUserUnlocked = true;
+        SharedPreferences prefs = LauncherPrefs.getPrefs(mContext);
+        prefs.registerOnSharedPreferenceChangeListener(this);
         recreateTaskbar();
     }
 
