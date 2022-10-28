5f65f94210cb95cc6aa946763d74ae72cea40a4c
Pranav Vashi <neobuddy89@gmail.com>
2022-10-28 23:18:18 +0530

    Launcher3: Refresh themed icons after OTA update
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/com/android/launcher3/icons/LauncherIconProvider.java
+++ src/com/android/launcher3/icons/LauncherIconProvider.java
@@ -18,6 +18,7 @@ package com.android.launcher3.icons;
 import android.content.Context;
 import android.content.res.Resources;
 import android.content.res.XmlResourceParser;
+import android.os.Build;
 import android.text.TextUtils;
 import android.util.ArrayMap;
 import android.util.Log;
@@ -67,7 +68,8 @@ public class LauncherIconProvider extends IconProvider {
 
     @Override
     public String getSystemIconState() {
-        return super.getSystemIconState() + (mSupportsIconTheme ? ",with-theme" : ",no-theme");
+        return super.getSystemIconState() + (mSupportsIconTheme ? ",with-theme" : ",no-theme")
+                + "," + Build.VERSION.INCREMENTAL;
     }
 
     private Map<String, ThemeData> getThemedIconMap() {
