badd93df903a5bb54e7c1a967266f33caf6d1b9a
Pranav Vashi <neobuddy89@gmail.com>
2022-11-03 00:11:09 +0530

    Revert "Launcher3: Use a custom icon factory to decide icon wrapping"
    This reverts commit 2bba44991dfbfd5f356038f34a1125eabb387792.

--- src/com/android/launcher3/lineage/icon/LineageIconFactory.java
+++ src/com/android/launcher3/lineage/icon/LineageIconFactory.java
@@ -16,6 +16,7 @@
  */
 package com.android.launcher3.lineage.icon;
 
+
 import static android.graphics.Paint.DITHER_FLAG;
 import static android.graphics.Paint.FILTER_BITMAP_FLAG;
 
@@ -42,27 +43,37 @@ import android.os.UserHandle;
 import androidx.annotation.NonNull;
 
 import com.android.launcher3.icons.BaseIconFactory;
-import com.android.launcher3.icons.BitmapInfo;
-import com.android.launcher3.icons.FixedScaleDrawable;
-import com.android.launcher3.icons.ShadowGenerator;
 
-public class LineageIconFactory extends BaseIconFactory {
-
+public class LineageIconFactory /* extends BaseIconFactory  */{
+/*
+    private final Context mContext;
+    private final PackageManager mPm;
     private final IconPackStore mIconPackStore;
 
-    public LineageIconFactory(Context context, int fillResIconDpi, int iconBitmapSize) {
-        super(context, fillResIconDpi, iconBitmapSize);
-        mIconPackStore = new IconPackStore(context);
-    }
-
-    public LineageIconFactory(Context context, int fillResIconDpi, int iconBitmapSize,
-            boolean shapeDetection) {
-        super(context, fillResIconDpi, iconBitmapSize, shapeDetection);
+    public LineageIconFactory(Context context) {
+        mContext = context;
+        mPm = context.getPackageManager();
         mIconPackStore = new IconPackStore(context);
     }
 
     @Override
-    protected Drawable normalizeAndWrapToAdaptiveIcon(@NonNull Drawable icon,
+     public BitmapInfo createBadgedIconBitmap(@NonNull Drawable icon, UserHandle user,
+            boolean shrinkNonAdaptiveIcons, boolean isInstantApp, float[] scale) {
+        if (scale == null) {
+            scale = new float[1];
+        }
+    }
+
+    @Override
+    public Bitmap createScaledBitmapWithoutShadow(Drawable icon, boolean shrinkNonAdaptiveIcons) {
+        RectF iconBounds = new RectF();
+        float[] scale = new float[1];
+        icon = normalizeAndWrapToAdaptiveIcon(icon, shrinkNonAdaptiveIcons, iconBounds, scale);
+        return createIconBitmap(icon,
+                Math.min(scale[0], ShadowGenerator.getScaleForBounds(iconBounds)));
+    }
+
+    private Drawable normalizeAndWrapToAdaptiveIcon(@NonNull Drawable icon,
             boolean shrinkNonAdaptiveIcons, RectF outIconBounds, float[] outScale) {
         if (icon == null) {
             return null;
@@ -72,7 +83,7 @@ public class LineageIconFactory extends BaseIconFactory {
         final boolean defaultIcons = mIconPackStore.isUsingSystemIcons();
         if (shrinkNonAdaptiveIcons && defaultIcons) {
             if (mWrapperIcon == null) {
-                mWrapperIcon = mContext.getDrawable(com.android.launcher3.icons.R.drawable.adaptive_icon_drawable_wrapper)
+                mWrapperIcon = mContext.getDrawable(R.drawable.adaptive_icon_drawable_wrapper)
                         .mutate();
             }
             AdaptiveIconDrawable dr = (AdaptiveIconDrawable) mWrapperIcon;
@@ -95,4 +106,5 @@ public class LineageIconFactory extends BaseIconFactory {
         outScale[0] = scale;
         return icon;
     }
+*/
 }
