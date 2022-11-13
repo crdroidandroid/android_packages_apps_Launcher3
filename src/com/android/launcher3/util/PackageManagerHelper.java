94095ee0c3654dd4847fc52a9a75bdb3bb7b43c0
Pranav Vashi <neobuddy89@gmail.com>
2022-11-13 13:50:05 +0530

    Launcher3: Switch to icon pack theming preview
    Ref:
    https://github.com/crdroidandroid/android_packages_apps_Launcher3/commit/8155a4f3db5888b578aecb8904529a22f962968e
    https://github.com/crdroidandroid/android_packages_apps_Launcher3/commit/174d11c6fa88e85920ec078af506fc56ef3bccd4
    
    Tweaked to our usage
    
    Change-Id: I82b16d0d4bf5f8e9ef1dca3dc64e4aa1714e04f1
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/com/android/launcher3/util/PackageManagerHelper.java
+++ src/com/android/launcher3/util/PackageManagerHelper.java
@@ -51,6 +51,7 @@ import com.android.launcher3.model.data.ItemInfoWithIcon;
 import com.android.launcher3.model.data.LauncherAppWidgetInfo;
 import com.android.launcher3.model.data.WorkspaceItemInfo;
 
+import java.net.URISyntaxException;
 import java.util.List;
 import java.util.Objects;
 
@@ -113,6 +114,22 @@ public class PackageManagerHelper {
             @NonNull final UserHandle user) {
         List<LauncherActivityInfo> activities = mLauncherApps.getActivityList(pkg, user);
         return activities.isEmpty() ? null : activities.get(0);
+    }
+
+    /**
+     * Creates a new market search intent.
+     */
+    public static Intent getMarketSearchIntent(Context context, String query) {
+        try {
+            Intent intent = Intent.parseUri("market://search?c=apps", 0);
+            if (!TextUtils.isEmpty(query)) {
+                intent.setData(
+                        intent.getData().buildUpon().appendQueryParameter("q", query).build());
+            }
+            return intent;
+        } catch (URISyntaxException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     /**
