becad7ae2c0fd75631ab98e53ceb14a14f19c5e5
AmeChanRain <downloadbot007@gmail.com>
2022-11-18 03:19:09 +0000

    Launcher3: Shake to clear all tasks
    * This allow users to clear all tasks on Task Overview by shaking phone
      instead of click 'Clear All' button.
    
    @neobuddy89: Squashed and few minor fixes added.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- quickstep/src/com/android/quickstep/views/OverviewActionsView.java
+++ quickstep/src/com/android/quickstep/views/OverviewActionsView.java
@@ -39,6 +39,7 @@ import com.android.launcher3.util.DisplayController;
 import com.android.launcher3.util.MultiPropertyFactory.MultiProperty;
 import com.android.launcher3.util.MultiValueAlpha;
 import com.android.launcher3.util.NavigationMode;
+import com.android.launcher3.util.ShakeUtils;
 import com.android.quickstep.TaskOverlayFactory.OverlayUICallbacks;
 import com.android.quickstep.util.LayoutUtils;
 
@@ -49,7 +50,7 @@ import java.lang.annotation.RetentionPolicy;
  * View for showing action buttons in Overview
  */
 public class OverviewActionsView<T extends OverlayUICallbacks> extends FrameLayout
-        implements OnClickListener, Insettable, SharedPreferences.OnSharedPreferenceChangeListener {
+        implements OnClickListener, Insettable, SharedPreferences.OnSharedPreferenceChangeListener, ShakeUtils.OnShakeListener {
 
     private final Rect mInsets = new Rect();
 
@@ -100,6 +101,7 @@ public class OverviewActionsView<T extends OverlayUICallbacks> extends FrameLayo
 
     private MultiValueAlpha mMultiValueAlpha;
     private Button mSplitButton;
+    private ShakeUtils mShakeUtils;
 
     @ActionsHiddenFlags
     private int mHiddenFlags;
@@ -141,11 +143,30 @@ public class OverviewActionsView<T extends OverlayUICallbacks> extends FrameLayo
         prefs.registerOnSharedPreferenceChangeListener(this);
     }
 
+    private void bindShake() {
+        mShakeUtils.bindShakeListener(this);
+    }
+
+    private void unBindShake() {
+        mShakeUtils.unBindShakeListener(this);
+    }
+
+    @Override
+    public void onVisibilityAggregated(boolean isVisible) {
+        super.onVisibilityAggregated(isVisible);
+        if (isVisible) {
+            bindShake();
+        } else {
+            unBindShake();
+        }
+    }
+
     @Override
     protected void onFinishInflate() {
         super.onFinishInflate();
         mMultiValueAlpha = new MultiValueAlpha(findViewById(R.id.action_buttons), NUM_ALPHAS);
         mMultiValueAlpha.setUpdateVisibility(true);
+        mShakeUtils = new ShakeUtils(getContext());
         updateVisibilities();
     }
 
@@ -167,6 +188,14 @@ public class OverviewActionsView<T extends OverlayUICallbacks> extends FrameLayo
 
         mSplitButton = findViewById(R.id.action_split);
         mSplitButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onShake(double speed) {
+        if (mCallbacks != null && findViewById(R.id.action_clear_all).getVisibility() == VISIBLE) {
+            mCallbacks.onClearAllTasksRequested();
+            setCallbacks(null); // Clear the listener after shake
+        }
     }
 
     /**
