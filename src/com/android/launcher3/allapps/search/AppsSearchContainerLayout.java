3151a4810c31e61a9fc935756632806055f2046f
Pranav Vashi <neobuddy89@gmail.com>
2023-01-19 19:36:45 +0530

    Launcher3: Apply corner radius setting for app searchbar
    Co-authored-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/com/android/launcher3/allapps/search/AppsSearchContainerLayout.java
+++ src/com/android/launcher3/allapps/search/AppsSearchContainerLayout.java
@@ -23,8 +23,11 @@ import static com.android.launcher3.Utilities.prefixTextWithIcon;
 import static com.android.launcher3.icons.IconNormalizer.ICON_VISIBLE_AREA_FACTOR;
 
 import android.content.Context;
+import android.content.res.Resources;
 import android.content.Intent;
 import android.graphics.Rect;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.PaintDrawable;
 import android.text.Selection;
 import android.text.SpannableStringBuilder;
 import android.text.method.TextKeyListener;
@@ -36,7 +39,9 @@ import android.view.ViewGroup.MarginLayoutParams;
 import com.android.launcher3.DeviceProfile;
 import com.android.launcher3.ExtendedEditText;
 import com.android.launcher3.Insettable;
+import com.android.launcher3.LauncherPrefs;
 import com.android.launcher3.R;
+import com.android.launcher3.Utilities;
 import com.android.launcher3.allapps.ActivityAllAppsContainerView;
 import com.android.launcher3.allapps.AllAppsStore;
 import com.android.launcher3.allapps.BaseAllAppsAdapter.AdapterItem;
@@ -44,6 +49,7 @@ import com.android.launcher3.allapps.PrivateProfileManager;
 import com.android.launcher3.allapps.SearchUiManager;
 import com.android.launcher3.search.SearchCallback;
 import com.android.launcher3.util.ApiWrapper;
+import com.android.launcher3.util.Themes;
 import com.android.launcher3.views.ActivityContext;
 
 import java.util.ArrayList;
@@ -121,6 +127,10 @@ public class AppsSearchContainerLayout extends ExtendedEditText
     protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
         super.onLayout(changed, left, top, right, bottom);
 
+        Drawable gIcon = getContext().getDrawable(R.drawable.ic_super_g_color);
+        Drawable gIconThemed = getContext().getDrawable(R.drawable.ic_super_g_themed);
+        Drawable sIcon = getContext().getDrawable(R.drawable.ic_allapps_search);
+
         // Shift the widget horizontally so that its centered in the parent (b/63428078)
         View parent = (View) getParent();
         int availableWidth = parent.getWidth() - parent.getPaddingLeft() - parent.getPaddingRight();
@@ -129,7 +139,37 @@ public class AppsSearchContainerLayout extends ExtendedEditText
         int shift = expectedLeft - left;
         setTranslationX(shift);
 
+        if (Utilities.showQSB(getContext()) && !LauncherPrefs.DOCK_THEME.get(getContext())) {
+            setCompoundDrawablesRelativeWithIntrinsicBounds(gIcon, null, null, null);
+        } else if (Utilities.showQSB(getContext()) && LauncherPrefs.DOCK_THEME.get(getContext())) {
+            setCompoundDrawablesRelativeWithIntrinsicBounds(gIconThemed, null, null, null);
+        } else {
+            setCompoundDrawablesRelativeWithIntrinsicBounds(sIcon, null, null, null);
+        }
+
         offsetTopAndBottom(mContentOverlap);
+
+        setUpBackground();
+    }
+
+    private void setUpBackground() {
+        Context context = getContext();
+        float cornerRadius = getCornerRadius(context);
+        int color = Themes.getAttrColor(context, R.attr.qsbFillColor);
+        if (LauncherPrefs.DOCK_THEME.get(context))
+            color = Themes.getAttrColor(context, R.attr.qsbFillColorThemed);
+        PaintDrawable pd = new PaintDrawable(color);
+        pd.setCornerRadius(cornerRadius);
+        setClipToOutline(cornerRadius > 0);
+        setBackground(pd);
+    }
+
+    private float getCornerRadius(Context context) {
+        Resources res = context.getResources();
+        float qsbWidgetHeight = res.getDimension(R.dimen.qsb_widget_height);
+        float qsbWidgetPadding = res.getDimension(R.dimen.qsb_widget_vertical_padding);
+        float innerHeight = qsbWidgetHeight - 2 * qsbWidgetPadding;
+        return (innerHeight / 2) * ((float)LauncherPrefs.SEARCH_RADIUS_SIZE.get(context) / 100f);
     }
 
     @Override
--- src/com/android/launcher3/qsb/QsbLayout.java
+++ src/com/android/launcher3/qsb/QsbLayout.java
@@ -82,7 +82,7 @@ public class QsbLayout extends FrameLayout {
         float cornerRadius = getCornerRadius();
         int color = Themes.getAttrColor(mContext, R.attr.qsbFillColor);
         if (LauncherPrefs.DOCK_THEME.get(mContext))
-            color = Themes.getColorBackgroundFloating(mContext);
+            color = Themes.getAttrColor(mContext, R.attr.qsbFillColorThemed);
         PaintDrawable pd = new PaintDrawable(color);
         pd.setCornerRadius(cornerRadius);
         inner.setClipToOutline(cornerRadius > 0);
