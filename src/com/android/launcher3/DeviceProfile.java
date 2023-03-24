4c1971179c07a8f341b4ff803754376efd82f04a
Nauval Rizky <enuma.alrizky@gmail.com>
2023-03-25 01:56:22 +0700

    Launcher3: Account hotseatBorderSpace into qsbWidth and hotseat padding
    [cyberknight777: Simplify hotseatAdjustment value allocation]
    Change-Id: I9db4b64ae3b04074ced801e923619d0616c8e618
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/com/android/launcher3/DeviceProfile.java
+++ src/com/android/launcher3/DeviceProfile.java
@@ -826,7 +826,8 @@ public class DeviceProfile {
                     - iconExtraSpacePx;
         } else {
             int columns = inv.hotseatColumnSpan[mTypeIndex];
-            return getIconToIconWidthForColumns(columns) - iconExtraSpacePx;
+            return getIconToIconWidthForColumns(columns) - iconExtraSpacePx
+                    - hotseatBorderSpace;
         }
     }
 
@@ -1816,7 +1817,7 @@ public class DeviceProfile {
             // workspace cell vs a hotseat cell.
             float workspaceCellWidth = (float) widthPx / inv.numColumns;
             float hotseatCellWidth = (float) widthPx / numShownHotseatIcons;
-            int hotseatAdjustment = Math.round((workspaceCellWidth - hotseatCellWidth) / 2);
+            int hotseatAdjustment = hotseatBorderSpace / 2;
             hotseatBarPadding.set(
                     hotseatAdjustment + workspacePadding.left + cellLayoutPaddingPx.left
                             + mInsets.left,
