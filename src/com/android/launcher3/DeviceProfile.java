a2228559a67ce223abbf356c590dc418fb21adfc
Pranav Vashi <neobuddy89@gmail.com>
2023-03-30 17:02:14 +0530

    Launcher3: Prevent clipping by row height reduction
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/com/android/launcher3/DeviceProfile.java
+++ src/com/android/launcher3/DeviceProfile.java
@@ -1252,6 +1252,12 @@ public class DeviceProfile {
             allAppsCellHeightPx = (int) (drawerWidth / inv.numAllAppsColumns * allAppsCellHeightMultiplier);
             allAppsIconDrawablePaddingPx = 0;
         }
+        int cellContentHeight = allAppsIconSizePx + allAppsBorderSpacePx.y
+                + (allAppsIconText ? Utilities.calculateTextHeight(allAppsIconTextSizePx) : 0);
+        if (allAppsCellHeightPx < cellContentHeight) {
+            // Increase allAppsCellHeight to fit its content.
+            allAppsCellHeightPx = cellContentHeight;
+        }
         updateAllAppsContainerWidth();
         if (isVerticalBarLayout()) {
             hideWorkspaceLabelsIfNotEnoughSpace();
@@ -1329,13 +1335,6 @@ public class DeviceProfile {
                     allAppsIconSizePx = Math.min(allAppsIconSizePx, allAppsCellWidthPx);
                     allAppsBorderSpacePx.x = 0;
                 }
-            }
-
-            int cellContentHeight = allAppsIconSizePx
-                    + Utilities.calculateTextHeight(allAppsIconTextSizePx) + allAppsBorderSpacePx.y;
-            if (allAppsCellHeightPx < cellContentHeight) {
-                // Increase allAppsCellHeight to fit its content.
-                allAppsCellHeightPx = cellContentHeight;
             }
         } else {
             float invIconSizeDp = inv.allAppsIconSize[mTypeIndex];
