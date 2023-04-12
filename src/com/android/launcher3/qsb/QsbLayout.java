9bad79e6b83e1ae4c18fdd8bfdfc041c5be7446d
songlongdaiduong <song.long.dai.duong@gmail.com>
2023-04-12 14:28:11 +0800

    Launcher3: Adjust QSB width to align with hotseat
    Signed-off-by: Mohammad Hasan Keramat J <ikeramat@protonmail.com>
    Change-Id: Ieb32ffbff46b0f4ba12711a3ddba3576dde8517f
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/com/android/launcher3/qsb/QsbLayout.java
+++ src/com/android/launcher3/qsb/QsbLayout.java
@@ -16,8 +16,6 @@ import android.util.AttributeSet;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
 import androidx.core.view.ViewCompat;
-import com.android.launcher3.BaseActivity;
-import com.android.launcher3.DeviceProfile;
 import com.android.launcher3.LauncherPrefs;
 import com.android.launcher3.R;
 import com.android.launcher3.Reorderable;
@@ -25,7 +23,6 @@ import com.android.launcher3.Utilities;
 import com.android.launcher3.qsb.QsbContainerView;
 import com.android.launcher3.util.MultiTranslateDelegate;
 import com.android.launcher3.util.Themes;
-import com.android.launcher3.views.ActivityContext;
 import android.view.View;
 
 public class QsbLayout extends FrameLayout implements Reorderable {
@@ -116,14 +113,9 @@ public class QsbLayout extends FrameLayout implements Reorderable {
 
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-        int requestedWidth = MeasureSpec.getSize(widthMeasureSpec);
+        int width = MeasureSpec.getSize(widthMeasureSpec);
         int height = MeasureSpec.getSize(heightMeasureSpec);
 
-        DeviceProfile dp = ActivityContext.lookupContext(mContext).getDeviceProfile();
-        int cellWidth = DeviceProfile.calculateCellWidth(requestedWidth,
-            dp.getWorkspaceIconProfile().getCellLayoutBorderSpacePx().x, dp.numShownHotseatIcons);
-        int iconSize = (int)(Math.round((dp.getWorkspaceIconProfile().getIconSizePx() * 0.92f)));
-        int width = requestedWidth;
         setMeasuredDimension(width, height);
 
         for (int i = 0; i < getChildCount(); i++) {
