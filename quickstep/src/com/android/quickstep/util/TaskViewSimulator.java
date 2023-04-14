89b81592957b1d855d40fae1126d43e570cbf1c1
Anle Pan <anle.pan@nxp.com>
2023-04-14 14:13:23 +0000

    Launcher3: Fix monkey crash in createSurfaceParams
    it will appear a null object reference crash in
    quickstep lanch when monkey test, make a non-null
    judgement before use to avoid crash
    
    Change-Id: I0d92a9c3fa4d5836247b40c96a262c35eb6c1ea9
    Signed-off-by: Anle Pan <anle.pan@nxp.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- quickstep/src/com/android/quickstep/util/TaskViewSimulator.java
+++ quickstep/src/com/android/quickstep/util/TaskViewSimulator.java
@@ -503,9 +503,11 @@ public class TaskViewSimulator implements TransformParams.BuilderProxy {
             mTempRectF.roundOut(mTmpCropRect);
         }
 
-        params.setProgress(1f - fullScreenProgress);
-        params.applySurfaceParams(surfaceTransaction == null
-                ? params.createSurfaceParams(this) : surfaceTransaction);
+        if (params.getTargetSet() != null) {
+            params.setProgress(1f - fullScreenProgress);
+            params.applySurfaceParams(surfaceTransaction == null
+                    ? params.createSurfaceParams(this) : surfaceTransaction);
+        }
 
         if (!DEBUG) {
             return;
