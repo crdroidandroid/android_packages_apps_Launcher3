35f20fefd0f104aee085d9568de816e4038a9f6c
minaripenguin <minaripenguin@users.noreply.github.com>
2023-04-18 11:45:07 +0800

    Launcher3: Show a toast when restarting the launcher
    * Some users were reporting the launcher restart after config change as bug. Show a toast to let the user know its intended.
    
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    [@neobuddy89: Update strings.]
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/com/android/launcher3/LauncherAppState.java
+++ src/com/android/launcher3/LauncherAppState.java
@@ -30,6 +30,7 @@ import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
 import android.content.pm.LauncherApps;
 import android.os.UserHandle;
 import android.util.Log;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 
@@ -166,6 +167,7 @@ public class LauncherAppState implements SafeCloseable {
         // we destroyed Settings activity with the back button
         // so we force a restart now if needed without waiting for home button press
         if (mNeedsRestart) {
+            Toast.makeText(mContext, R.string.restarting_launcher_changes, Toast.LENGTH_SHORT).show();
             Utilities.restart(mContext);
         }
     }
