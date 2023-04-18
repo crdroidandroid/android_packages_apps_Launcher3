8797f2878b227dfd1602b6ee9b572462f7872ad6
minaripenguin <minaripenguin@users.noreply.github.com>
2023-04-18 11:45:07 +0800

    Launcher3: Show a toast when restarting the launcher
    * Some users were reporting the launcher restart after config change as bug. Show a toast to let the user know its intended.
    
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    [@neobuddy89: Update strings.]
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/com/android/launcher3/model/PackageUpdatedTask.java
+++ src/com/android/launcher3/model/PackageUpdatedTask.java
@@ -32,6 +32,7 @@ import android.content.pm.LauncherApps;
 import android.content.pm.ShortcutInfo;
 import android.os.UserHandle;
 import android.os.UserManager;
+import android.widget.Toast;
 import android.util.Log;
 
 import androidx.annotation.NonNull;
@@ -39,6 +40,7 @@ import androidx.annotation.NonNull;
 import com.android.launcher3.Flags;
 import com.android.launcher3.LauncherModel.ModelUpdateTask;
 import com.android.launcher3.LauncherSettings.Favorites;
+import com.android.launcher3.R;
 import com.android.launcher3.Utilities;
 import com.android.launcher3.config.FeatureFlags;
 import com.android.launcher3.icons.IconCache;
@@ -445,6 +447,7 @@ public class PackageUpdatedTask implements ModelUpdateTask {
         }
 
         if (needsRestart) {
+            Toast.makeText(context, R.string.updating_launcher_components, Toast.LENGTH_SHORT).show();
             Utilities.restart(context);
         }
     }
