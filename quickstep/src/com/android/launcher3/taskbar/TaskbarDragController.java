82650b40a1ac7ec62691c1ebc99d92c2b8f1fa4c
Pranav Vashi <neobuddy89@gmail.com>
2023-04-22 23:04:56 +0530

    Launcher3: Add NPE check in TaskbarDragController
    Fixes: https://github.com/crdroidandroid/issue_tracker/issues/173
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- quickstep/src/com/android/launcher3/taskbar/TaskbarDragController.java
+++ quickstep/src/com/android/launcher3/taskbar/TaskbarDragController.java
@@ -446,7 +446,7 @@ public class TaskbarDragController extends DragController<BaseTaskbarContext> im
     }
 
     private void maybeOnDragEnd() {
-        if (!isDragging()) {
+        if (mDragObject != null && !isDragging()) {
             ((BubbleTextView) mDragObject.originalView).getIcon().setIsDisabled(false);
             mControllers.taskbarAutohideSuspendController.updateFlag(
                     TaskbarAutohideSuspendController.FLAG_AUTOHIDE_SUSPEND_DRAGGING, false);
@@ -456,7 +456,7 @@ public class TaskbarDragController extends DragController<BaseTaskbarContext> im
 
     @Override
     protected void endDrag() {
-        if (mDisallowGlobalDrag) {
+        if (mDragObject != null && mDisallowGlobalDrag) {
             // We need to explicitly set deferDragViewCleanupPostAnimation to true here so the
             // super call doesn't remove it from the drag layer before the animation completes.
             // This variable gets set in to false in super.dispatchDropComplete() because it
