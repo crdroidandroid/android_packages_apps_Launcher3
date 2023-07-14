c4d4767cc5f4bd937148bb2e91f715537070f7ce
hmtheboy154 <buingoc67@gmail.com>
2023-07-14 05:21:02 -0400

    Launcher3: Update meminfo margin when using taskbar
    On tablet, when meminfo is at the bottom, the margin only set to 15dp,
    so you can't be able to see it when you are using a tablet with taskbar
    enabled.
    
    @neobuddy89:
    * Keep dimens only for transient taskbar
    * Tune dimens for 3-button navbar
    
    Change-Id: Id5968736b44628cf69fed0fe76fd3ee47e621a00
    Signed-off-by: hmtheboy154 <buingoc67@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/com/android/launcher3/DeviceProfile.java
+++ src/com/android/launcher3/DeviceProfile.java
@@ -196,6 +196,7 @@ public class DeviceProfile {
 
     // Meminfo in overview
     public int memInfoMarginGesturePx;
+    public int memInfoMarginTransientTaskbarPx;
     public int memInfoMarginThreeButtonPx;
 
     /** Used only as an alternative to mocking when null values cannot be used. */
@@ -503,6 +504,8 @@ public class DeviceProfile {
 
         memInfoMarginGesturePx = res.getDimensionPixelSize(
                 R.dimen.meminfo_bottom_margin_gesture);
+        memInfoMarginTransientTaskbarPx = res.getDimensionPixelSize(
+                R.dimen.meminfo_bottom_margin_transient_taskbar);
         memInfoMarginThreeButtonPx = res.getDimensionPixelSize(
                 R.dimen.meminfo_bottom_margin_three_button);
 
