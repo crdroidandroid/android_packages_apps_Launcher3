1216443206e07a45a2a6387d799446d7a78cabe7
nift4 <nift4@protonmail.com>
2023-08-07 19:42:24 +0200

    Launcher3: Respect l/r cutout in tablet app drawer too
    some people like to use sw600dp UI in phones...
    
    Change-Id: I83c8ec7a359929f1589c0ca35896f07bdcd7cecc
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/com/android/launcher3/allapps/ActivityAllAppsContainerView.java
+++ src/com/android/launcher3/allapps/ActivityAllAppsContainerView.java
@@ -1410,6 +1410,7 @@ public class ActivityAllAppsContainerView<T extends Context & ActivityContext>
     @Override
     public void drawOnScrimWithScaleAndBottomOffset(
             Canvas canvas, float scale, @Px int bottomOffsetPx) {
+        final MarginLayoutParams mlp = (MarginLayoutParams) getLayoutParams();
         final View panel = mBottomSheetBackground;
         final boolean hasBottomSheet = panel.getVisibility() == VISIBLE;
         final float translationY = ((View) panel.getParent()).getTranslationY();
@@ -1419,8 +1420,8 @@ public class ActivityAllAppsContainerView<T extends Context & ActivityContext>
 
         final float topNoScale = panel.getTop() + translationY;
         final float topWithScale = topNoScale + verticalScaleOffset;
-        final float leftWithScale = panel.getLeft() + horizontalScaleOffset;
-        final float rightWithScale = panel.getRight() - horizontalScaleOffset;
+        final float leftWithScale = mlp.leftMargin + panel.getLeft() + horizontalScaleOffset;
+        final float rightWithScale = mlp.leftMargin + panel.getRight() - mlp.rightMargin - horizontalScaleOffset;
         final float bottomWithOffset = panel.getBottom() + bottomOffsetPx;
         // Draw full background panel for tablets.
         if (hasBottomSheet) {
