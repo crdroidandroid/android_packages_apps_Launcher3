e6e1d3c5ea245f786060cbdd6a67c8f8790d64b5
minaripenguin <minaripenguin@users.noreply.github.com>
2023-09-17 00:09:38 +0800

    Launcher3: Launch apps in freeform mode system shortcut
    Change-Id: I3b68d38509cbe45890bfab49435be2e08ea04e26
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- quickstep/src/com/android/launcher3/uioverrides/QuickstepLauncher.java
+++ quickstep/src/com/android/launcher3/uioverrides/QuickstepLauncher.java
@@ -45,6 +45,7 @@ import static com.android.launcher3.model.data.ItemInfo.NO_MATCHING_ID;
 import static com.android.launcher3.popup.QuickstepSystemShortcut.getSplitSelectShortcutByPosition;
 import static com.android.launcher3.popup.SystemShortcut.APP_INFO;
 import static com.android.launcher3.popup.SystemShortcut.DONT_SUGGEST_APP;
+import static com.android.launcher3.popup.SystemShortcut.FREE_FORM;
 import static com.android.launcher3.popup.SystemShortcut.INSTALL;
 import static com.android.launcher3.popup.SystemShortcut.PAUSE_APPS;
 import static com.android.launcher3.popup.SystemShortcut.PRIVATE_PROFILE_INSTALL;
@@ -471,6 +472,7 @@ public class QuickstepLauncher extends Launcher {
         shortcuts.addAll(getSplitShortcuts());
         shortcuts.add(UNINSTALL);
         shortcuts.add(PAUSE_APPS);
+        shortcuts.add(FREE_FORM);
         shortcuts.add(WIDGETS);
         shortcuts.add(INSTALL);
         if (Flags.enablePrivateSpaceInstallShortcut()) {
--- src/com/android/launcher3/popup/SystemShortcut.java
+++ src/com/android/launcher3/popup/SystemShortcut.java
@@ -1,5 +1,6 @@
 package com.android.launcher3.popup;
 
+import static android.app.WindowConfiguration.WINDOWING_MODE_FREEFORM;
 import static android.content.pm.SuspendDialogInfo.BUTTON_ACTION_UNSUSPEND;
 
 import static com.android.launcher3.logging.StatsLogManager.LauncherEvent.LAUNCHER_PRIVATE_SPACE_INSTALL_SYSTEM_SHORTCUT_TAP;
@@ -10,6 +11,8 @@ import static com.android.launcher3.logging.StatsLogManager.LauncherEvent.LAUNCH
 import static com.android.launcher3.LauncherSettings.Favorites.ITEM_TYPE_APPLICATION;
 import static com.android.launcher3.LauncherSettings.Favorites.ITEM_TYPE_TASK;
 
+import android.app.Activity;
+import android.app.ActivityManagerNative;
 import android.app.ActivityOptions;
 import android.app.AlertDialog;
 import android.app.AppGlobals;
@@ -29,6 +32,7 @@ import android.os.UserHandle;
 import android.util.Log;
 import android.view.InflateException;
 import android.view.View;
+import android.view.WindowInsets;
 import android.view.accessibility.AccessibilityNodeInfo;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -523,6 +527,41 @@ public abstract class SystemShortcut<T extends ActivityContext> extends ItemInfo
             } catch (URISyntaxException e) {
                 // Do nothing.
             }
+        }
+    }
+
+    public static final Factory<ActivityContext> FREE_FORM = (activity, itemInfo, originalView) -> 
+        new FreeForm(activity, itemInfo, originalView);
+
+    public static class FreeForm<T extends ActivityContext> extends SystemShortcut<T> { 
+        private final String mPackageName;
+        
+        public FreeForm(T target, ItemInfo itemInfo, View originalView) {
+            super(R.drawable.ic_caption_desktop_button_foreground, R.string.recent_task_option_freeform, target, itemInfo, originalView);
+            mPackageName = itemInfo.getTargetComponent().getPackageName();
+        }
+
+        @Override
+        public void onClick(View view) {
+            if (mPackageName != null) {
+                Intent intent = ((Context) mTarget).getPackageManager().getLaunchIntentForPackage(mPackageName);
+                if (intent != null) {
+                    ActivityOptions options = makeLaunchOptions(((Activity) mTarget));
+                    ((Context) mTarget).startActivity(intent, options.toBundle());
+                    AbstractFloatingView.closeAllOpenViews(((ActivityContext) mTarget));
+                }
+            }
+        }
+
+        private ActivityOptions makeLaunchOptions(Activity activity) {
+            ActivityOptions activityOptions = ActivityOptions.makeBasic();
+            activityOptions.setLaunchWindowingMode(WINDOWING_MODE_FREEFORM);
+            final View decorView = activity.getWindow().getDecorView();
+            final WindowInsets insets = decorView.getRootWindowInsets();
+            final Rect r = new Rect(0, 0, decorView.getWidth() / 2, decorView.getHeight() / 2);
+            r.offsetTo(insets.getSystemWindowInsetLeft() + 50, insets.getSystemWindowInsetTop() + 50);
+            activityOptions.setLaunchBounds(r);
+            return activityOptions;
         }
     }
 
