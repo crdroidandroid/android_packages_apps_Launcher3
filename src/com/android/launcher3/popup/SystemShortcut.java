7d031bcd491d9011b529c276f5e9bcc7160b8383
minaripenguin <minaripenguin@users.noreply.github.com>
2023-09-17 11:46:41 +0800

    Launcher3: freeform: Launch tasks as tasks overlays
    * we want to make the activity always the top activity for the launched task. Following OEM multi-window behaviour.
    
    Change-Id: I5d03dac91ad5731065c6b44c94f7ddac585c9091
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/com/android/launcher3/popup/SystemShortcut.java
+++ src/com/android/launcher3/popup/SystemShortcut.java
@@ -37,6 +37,7 @@ import android.view.accessibility.AccessibilityNodeInfo;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.window.SplashScreen;
 import android.os.UserHandle;
 
 import androidx.annotation.NonNull;
@@ -561,6 +562,8 @@ public abstract class SystemShortcut<T extends ActivityContext> extends ItemInfo
             final Rect r = new Rect(0, 0, decorView.getWidth() / 2, decorView.getHeight() / 2);
             r.offsetTo(insets.getSystemWindowInsetLeft() + 50, insets.getSystemWindowInsetTop() + 50);
             activityOptions.setLaunchBounds(r);
+            activityOptions.setSplashScreenStyle(SplashScreen.SPLASH_SCREEN_STYLE_ICON);
+            activityOptions.setTaskOverlay(true /* taskOverlay */, true /* canResume */);
             return activityOptions;
         }
     }
