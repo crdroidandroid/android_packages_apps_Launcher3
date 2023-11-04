0ee6ea124444d82076e69bf4cc7e453e77874bb0
Adithya R <gh0strider.2k18.reborn@gmail.com>
2023-11-04 17:21:51 +0530

    Launcher3: Start lens activity directly instead of sharesheet
    We don't need to open the sharesheet as we're targeting a specific
    application (Google Lens).
    
    Change-Id: I042b3a712dcf03eda21504b66cf99394b0bd4d2b
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- quickstep/src/com/android/quickstep/util/ImageActionUtils.java
+++ quickstep/src/com/android/quickstep/util/ImageActionUtils.java
@@ -163,15 +163,28 @@ public class ImageActionUtils {
 
     @WorkerThread
     public static void startLensActivity(Context context, Supplier<Bitmap> bitmapSupplier,
-            Rect crop, Intent intent, String tag) {
-        if (bitmapSupplier.get() == null) {
-            Log.e(tag, "No snapshot available, not starting share.");
-            return;
-        }
-
-        UI_HELPER_EXECUTOR.execute(() -> persistBitmapAndStartActivity(context,
-                bitmapSupplier.get(), crop, intent, ImageActionUtils::getLensIntentForImageUri,
-                tag));
+            Rect crop, String tag) {
+        UI_HELPER_EXECUTOR.execute(() -> {
+            Bitmap bitmap = bitmapSupplier.get();
+            if (bitmap == null) {
+                Log.e(tag, "No snapshot available, not starting lens.");
+                return;
+            }
+            Intent intent = new Intent();
+            Uri uri = getImageUri(bitmap, crop, context, tag);
+            ClipData clipdata = new ClipData(new ClipDescription("content",
+                    new String[]{"image/png"}),
+                    new ClipData.Item(uri));
+            intent.setAction(Intent.ACTION_SEND)
+                    .setComponent(
+                            new ComponentName(Utilities.GSA_PACKAGE, Utilities.LENS_SHARE_ACTIVITY))
+                    .addFlags(FLAG_ACTIVITY_NEW_TASK)
+                    .addFlags(FLAG_GRANT_READ_URI_PERMISSION)
+                    .setType("image/png")
+                    .putExtra(Intent.EXTRA_STREAM, uri)
+                    .setClipData(clipdata);
+            context.startActivity(intent);
+        });
     }
 
     /**
@@ -306,24 +319,6 @@ public class ImageActionUtils {
                 new ClipData.Item(uri));
         intent.setAction(Intent.ACTION_SEND)
                 .setComponent(null)
-                .addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-                .addFlags(FLAG_GRANT_READ_URI_PERMISSION)
-                .setType("image/png")
-                .putExtra(Intent.EXTRA_STREAM, uri)
-                .setClipData(clipdata);
-        return new Intent[]{Intent.createChooser(intent, null).addFlags(FLAG_ACTIVITY_NEW_TASK)};
-    }
-
-    @WorkerThread
-    private static Intent[] getLensIntentForImageUri(Uri uri, Intent intent) {
-        if (intent == null) {
-            intent = new Intent();
-        }
-        ClipData clipdata = new ClipData(new ClipDescription("content",
-                new String[]{"image/png"}),
-                new ClipData.Item(uri));
-        intent.setAction(Intent.ACTION_SEND)
-                .setComponent(new ComponentName(Utilities.GSA_PACKAGE, Utilities.LENS_SHARE_ACTIVITY))
                 .addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
                 .addFlags(FLAG_GRANT_READ_URI_PERMISSION)
                 .setType("image/png")
