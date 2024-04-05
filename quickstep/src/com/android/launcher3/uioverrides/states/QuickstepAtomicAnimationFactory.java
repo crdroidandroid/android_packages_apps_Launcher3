b7084c8cf26d306e5dbbdee5b27bf47f35e4a5da
Ido Ben-Hur <idoybh2@gmail.com>
2024-04-05 13:11:53 +0300

    Launcher3: QuickstepAtomicAnimationFactory: Add some null checks to prevent NPEs
    Solves the following crash on exiting recents when nothing is on any
    home screen:
    FATAL EXCEPTION: main
    Process: com.android.launcher3, PID: 7671
    java.lang.NullPointerException: Attempt to invoke virtual method 'int android.view.ViewGroup.getVisibility()' on a null object reference
    	at com.android.launcher3.uioverrides.states.QuickstepAtomicAnimationFactory.prepareForAtomicAnimation(go/retraceme d64815e9bb1a1e22c3f9643b1a3046dfb4cece8716c9ab9e81a56f1b7725d51a:303)
    	at com.android.launcher3.statemanager.StateManager.goToStateAnimated(go/retraceme d64815e9bb1a1e22c3f9643b1a3046dfb4cece8716c9ab9e81a56f1b7725d51a:26)
    	at com.android.launcher3.statemanager.StateManager.goToState(go/retraceme d64815e9bb1a1e22c3f9643b1a3046dfb4cece8716c9ab9e81a56f1b7725d51a:23)
    	at com.android.launcher3.statemanager.StateManager.goToState(go/retraceme d64815e9bb1a1e22c3f9643b1a3046dfb4cece8716c9ab9e81a56f1b7725d51a:2)
    	at com.android.quickstep.views.LauncherRecentsView.handleStartHome(go/retraceme d64815e9bb1a1e22c3f9643b1a3046dfb4cece8716c9ab9e81a56f1b7725d51a:16)
    	at com.android.quickstep.views.RecentsView.startHome(go/retraceme d64815e9bb1a1e22c3f9643b1a3046dfb4cece8716c9ab9e81a56f1b7725d51a:3)
    	at com.android.quickstep.views.RecentsView.startHome(go/retraceme d64815e9bb1a1e22c3f9643b1a3046dfb4cece8716c9ab9e81a56f1b7725d51a:1)
    	at com.android.quickstep.views.RecentsView$21.onEnd(go/retraceme d64815e9bb1a1e22c3f9643b1a3046dfb4cece8716c9ab9e81a56f1b7725d51a:394)
    	at com.android.quickstep.views.RecentsView$21.accept(go/retraceme d64815e9bb1a1e22c3f9643b1a3046dfb4cece8716c9ab9e81a56f1b7725d51a:4)
    	at com.android.quickstep.views.RecentsView$21.accept(go/retraceme d64815e9bb1a1e22c3f9643b1a3046dfb4cece8716c9ab9e81a56f1b7725d51a:1)
    	at com.android.launcher3.anim.AnimatorListeners$EndStateCallbackWrapper.onAnimationEnd(go/retraceme d64815e9bb1a1e22c3f9643b1a3046dfb4cece8716c9ab9e81a56f1b7725d51a:32)
    	at com.android.launcher3.anim.AnimatorPlaybackController$$ExternalSyntheticLambda0.accept(go/retraceme d64815e9bb1a1e22c3f9643b1a3046dfb4cece8716c9ab9e81a56f1b7725d51a:14)
    	at com.android.launcher3.anim.AnimatorPlaybackController.$r8$lambda$bF6fu10ogo158poE8C5B_R0HpFg(go/retraceme d64815e9bb1a1e22c3f9643b1a3046dfb4cece8716c9ab9e81a56f1b7725d51a:27)
    	at com.android.launcher3.anim.AnimatorPlaybackController$$ExternalSyntheticLambda3.accept(go/retraceme d64815e9bb1a1e22c3f9643b1a3046dfb4cece8716c9ab9e81a56f1b7725d51a:12)
    	at com.android.launcher3.anim.AnimatorPlaybackController.callAnimatorCommandRecursively(go/retraceme d64815e9bb1a1e22c3f9643b1a3046dfb4cece8716c9ab9e81a56f1b7725d51a:1)
    	at com.android.launcher3.anim.AnimatorPlaybackController.callAnimatorCommandRecursively(go/retraceme d64815e9bb1a1e22c3f9643b1a3046dfb4cece8716c9ab9e81a56f1b7725d51a:36)
    	at com.android.launcher3.anim.AnimatorPlaybackController.callListenerCommandRecursively(go/retraceme d64815e9bb1a1e22c3f9643b1a3046dfb4cece8716c9ab9e81a56f1b7725d51a:7)
    	at com.android.launcher3.anim.AnimatorPlaybackController.dispatchOnEnd(go/retraceme d64815e9bb1a1e22c3f9643b1a3046dfb4cece8716c9ab9e81a56f1b7725d51a:9)
    	at com.android.launcher3.anim.AnimatorPlaybackController$OnAnimationEndDispatcher.onAnimationSuccess(go/retraceme d64815e9bb1a1e22c3f9643b1a3046dfb4cece8716c9ab9e81a56f1b7725d51a:7)
    	at com.android.launcher3.anim.AnimationSuccessListener.onAnimationEnd(go/retraceme d64815e9bb1a1e22c3f9643b1a3046dfb4cece8716c9ab9e81a56f1b7725d51a:5)
    ...

--- quickstep/src/com/android/launcher3/uioverrides/states/QuickstepAtomicAnimationFactory.java
+++ quickstep/src/com/android/launcher3/uioverrides/states/QuickstepAtomicAnimationFactory.java
@@ -153,20 +153,20 @@ public class QuickstepAtomicAnimationFactory extends
 
             Workspace<?> workspace = mContainer.getWorkspace();
             // Start from a higher workspace scale, but only if we're invisible so we don't jump.
-            boolean isWorkspaceVisible = workspace.getVisibility() == VISIBLE;
+            boolean isWorkspaceVisible = workspace != null && workspace.getVisibility() == VISIBLE;
             if (isWorkspaceVisible) {
                 CellLayout currentChild = (CellLayout) workspace.getChildAt(
                         workspace.getCurrentPage());
-                isWorkspaceVisible = currentChild.getVisibility() == VISIBLE
+                isWorkspaceVisible = currentChild != null && currentChild.getVisibility() == VISIBLE
                         && currentChild.getShortcutsAndWidgets().getAlpha() > 0;
             }
-            if (!isWorkspaceVisible) {
+            if (!isWorkspaceVisible && workspace != null) {
                 workspace.setScaleX(WORKSPACE_PREPARE_SCALE);
                 workspace.setScaleY(WORKSPACE_PREPARE_SCALE);
             }
             Hotseat hotseat = mContainer.getHotseat();
             boolean isHotseatVisible = hotseat.getVisibility() == VISIBLE && hotseat.getAlpha() > 0;
-            if (!isHotseatVisible) {
+            if (!isHotseatVisible && hotseat != null) {
                 hotseat.setScaleX(WORKSPACE_PREPARE_SCALE);
                 hotseat.setScaleY(WORKSPACE_PREPARE_SCALE);
             }
@@ -188,7 +188,7 @@ public class QuickstepAtomicAnimationFactory extends
                 config.setInterpolator(ANIM_OVERVIEW_FADE, OVERSHOOT_1_2);
 
                 // Scale up the recents, if it is not coming from the side
-                if (overview.getVisibility() != VISIBLE || overview.getContentAlpha() == 0) {
+                if (overview != null && overview.getVisibility() != VISIBLE || overview.getContentAlpha() == 0) {
                     RECENTS_SCALE_PROPERTY.set(overview, RECENTS_PREPARE_SCALE);
                 }
             }
