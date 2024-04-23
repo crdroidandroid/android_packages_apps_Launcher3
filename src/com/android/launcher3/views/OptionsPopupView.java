8533ccbf89202aaaf08f313eb0fd43c76453f2a1
minaripenguin <minaripenguin@users.noreply.github.com>
2024-04-24 07:58:03 +0800

    Launcher3: PopUpView: Restore legacy popup options order
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/com/android/launcher3/views/OptionsPopupView.java
+++ src/com/android/launcher3/views/OptionsPopupView.java
@@ -200,10 +200,10 @@ public class OptionsPopupView<T extends Context & ActivityContext> extends Arrow
     public static ArrayList<OptionItem> getOptions(Launcher launcher) {
         ArrayList<OptionItem> options = new ArrayList<>();
         options.add(new OptionItem(launcher,
-                R.string.styles_wallpaper_button_text,
-                R.drawable.ic_palette,
-                IGNORE,
-                OptionsPopupView::startWallpaperPicker));
+                R.string.settings_title,
+                R.drawable.ic_setting,
+                LAUNCHER_SETTINGS_BUTTON_TAP_OR_LONGPRESS,
+                OptionsPopupView::startSettings));
         if (WIDGETS_ENABLED && Utilities.isWorkspaceEditAllowed(launcher)) {
             options.add(new OptionItem(launcher,
                     R.string.widget_button_text,
@@ -219,10 +219,10 @@ public class OptionsPopupView<T extends Context & ActivityContext> extends Arrow
                     OptionsPopupView::enterHomeGardening));
         }
         options.add(new OptionItem(launcher,
-                R.string.settings_title,
-                R.drawable.ic_setting,
-                LAUNCHER_SETTINGS_BUTTON_TAP_OR_LONGPRESS,
-                OptionsPopupView::startSettings));
+                R.string.styles_wallpaper_button_text,
+                R.drawable.ic_palette,
+                IGNORE,
+                OptionsPopupView::startWallpaperPicker));
         return options;
     }
 
