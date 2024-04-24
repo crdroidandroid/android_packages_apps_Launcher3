4c322bd9bf8915ed5da91b5c9c89135e93f6a40c
Dhina17 <dhinalogu@gmail.com>
2024-04-24 16:49:24 +0000

    Launcher3: Fix pausing work apps from multiple profiles
    Right now, "Pause work apps" fab is pausing only the apps
    from first found work profile and leaving the apps from
    another work profiles.
    
    and even can't unpause the paused apps with the fab.
    
    Simply, "Pause work apps" fab is broken when mutliple work
    profiles are available.
    
    With this commit, "Pause work apps" fab will pause all work apps
    from multiple profiles.
    As an expected behaviour from this patch I564c4afb55f47e3d9463790b7c45b94b3303b39a.
    
    Change-Id: Ifa04537c8b46842616f4d7c7e4c3957777042305
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/com/android/launcher3/allapps/WorkProfileManager.java
+++ src/com/android/launcher3/allapps/WorkProfileManager.java
@@ -28,7 +28,9 @@ import static com.android.launcher3.model.BgDataModel.Callbacks.FLAG_HAS_SHORTCU
 import static com.android.launcher3.model.BgDataModel.Callbacks.FLAG_QUIET_MODE_CHANGE_PERMISSION;
 import static com.android.launcher3.model.BgDataModel.Callbacks.FLAG_QUIET_MODE_ENABLED;
 import static com.android.launcher3.model.BgDataModel.Callbacks.FLAG_WORK_PROFILE_QUIET_MODE_ENABLED;
+import static com.android.launcher3.util.Executors.UI_HELPER_EXECUTOR;
 
+import android.content.Context;
 import android.os.UserHandle;
 import android.os.UserManager;
 import android.util.Log;
@@ -65,10 +67,15 @@ public class WorkProfileManager extends UserProfileManager
 
     private boolean mHasMultipleProfiles;
 
+    private final UserManager mUserManager;
+    private final UserCache mUserCache;
+
     public WorkProfileManager(
             UserManager userManager, ActivityAllAppsContainerView allApps,
             StatsLogManager statsLogManager, UserCache userCache) {
         super(userManager, statsLogManager, userCache);
+        mUserManager = userManager;
+        mUserCache = userCache;
         mAllApps = allApps;
         mWorkProfileMatcher = (user) -> userCache.getUserInfo(user).isWork();
     }
@@ -254,5 +261,15 @@ public class WorkProfileManager extends UserProfileManager
     @Override
     public Predicate<UserHandle> getUserMatcher() {
         return mWorkProfileMatcher;
+    }
+
+    @Override
+    protected void setQuietMode(boolean enabled, Context context) {
+        UI_HELPER_EXECUTOR.post(() ->
+                mUserCache.getUserProfiles()
+                        .stream()
+                        .filter(getUserMatcher())
+                        .forEach(userHandle ->
+                                setQuietModeSafely(enabled, userHandle, context)));
     }
 }
