bf6a894aaff5398835d25837302e958cc6fc3881
Pranav Vashi <neobuddy89@gmail.com>
2024-06-29 01:26:12 +0530

    Launcher3: Add toggle to use two lines for app label
    Change-Id: I40c11b04995649611c215de3cf1955dd86a45f3f
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/com/android/launcher3/InvariantDeviceProfile.java
+++ src/com/android/launcher3/InvariantDeviceProfile.java
@@ -246,7 +246,6 @@ public class InvariantDeviceProfile {
     public int allAppsCellSpecsTwoPanelId = INVALID_RESOURCE_HANDLE;
 
     private String mLocale = "";
-    public boolean enableTwoLinesInAllApps = false;
 
     // If non-negative, the workspace row with which top of the all apps container is to be aligned
     // with.
@@ -309,15 +308,13 @@ public class InvariantDeviceProfile {
                     onConfigChanged(context);
                 }
                 Trace.endSection();
-            } else if (ENABLE_TWOLINE_ALLAPPS_TOGGLE.getSharedPrefKey().equals(key)
-                    && enableTwoLinesInAllApps != prefs.get(ENABLE_TWOLINE_ALLAPPS_TOGGLE)) {
-                onConfigChanged(context);
             } else if (ALLAPPS_THEMED_ICONS.getSharedPrefKey().equals(key) ||
                     DRAWER_OPEN_KEYBOARD.getSharedPrefKey().equals(key) ||
                     SHOW_DESKTOP_LABELS.getSharedPrefKey().equals(key) ||
                     SHOW_DRAWER_LABELS.getSharedPrefKey().equals(key) ||
                     ICON_SIZE.getSharedPrefKey().equals(key) ||
-                    FONT_SIZE.getSharedPrefKey().equals(key)) {
+                    FONT_SIZE.getSharedPrefKey().equals(key) ||
+                    ENABLE_TWOLINE_ALLAPPS_TOGGLE.getSharedPrefKey().equals(key)) {
                 onConfigChanged(context);
             }
         };
@@ -382,9 +379,6 @@ public class InvariantDeviceProfile {
     }
 
     private void initGrid(Context context, Info displayInfo, DisplayOption displayOption) {
-        enableTwoLinesInAllApps = Flags.enableTwolineToggle()
-                && Utilities.isEnglishLanguage(context)
-                && mPrefs.get(ENABLE_TWOLINE_ALLAPPS_TOGGLE);
         mLocale = context.getResources().getConfiguration().locale.toString();
 
         DisplayMetrics metrics = context.getResources().getDisplayMetrics();
