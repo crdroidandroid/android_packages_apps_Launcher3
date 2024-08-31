e0090f9ca29854574b1921a0a973045df05095df
Pranav Vashi <neobuddy89@gmail.com>
2024-08-31 22:25:39 +0530

    Launcher3: Allow setting transparency for hotseat search bar
    Ref: https://github.com/LawnchairLauncher/lawnchair/commit/8244819d65c45f642685d24d58fd1397fe54e32e
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/com/android/launcher3/Utilities.java
+++ src/com/android/launcher3/Utilities.java
@@ -180,6 +180,7 @@ public final class Utilities {
     public static final String KEY_SHORT_PARALLAX = "pref_short_parallax";
     public static final String KEY_SINGLE_PAGE_CENTER = "pref_single_page_center";
     public static final String KEY_DRAWER_SEARCH = "pref_drawer_search";
+    public static final String KEY_HOTSEAT_QSB_OPACITY = "pref_hotseat_qsb_opacity";
 
     /**
      * Returns true if theme is dark.
@@ -1121,5 +1122,10 @@ public final class Utilities {
     public static boolean showSearch(Context context) {
         SharedPreferences prefs = LauncherPrefs.getPrefs(context.getApplicationContext());
         return prefs.getBoolean(KEY_DRAWER_SEARCH, true);
+    }
+
+    public static int getHotseatQsbOpacity(Context context) {
+        SharedPreferences prefs = LauncherPrefs.getPrefs(context.getApplicationContext());
+        return prefs.getInt(KEY_HOTSEAT_QSB_OPACITY, 100);
     }
 }
--- src/com/android/launcher3/settings/SettingsHomescreen.java
+++ src/com/android/launcher3/settings/SettingsHomescreen.java
@@ -117,6 +117,7 @@ public class SettingsHomescreen extends CollapsingToolbarBaseActivity
             case Utilities.KEY_SHORT_PARALLAX:
             case Utilities.KEY_SINGLE_PAGE_CENTER:
             case Utilities.KEY_DOCK_MUSIC_SEARCH:
+            case Utilities.KEY_HOTSEAT_QSB_OPACITY:
                 LauncherAppState.INSTANCE.executeIfCreated(app -> app.setNeedsRestart());
                 break;
             default:
