138cc8822f4ba2627cd391fa18c44a8425db9379
Pranav Vashi <neobuddy89@gmail.com>
2024-12-09 23:04:42 +0530

    Launcher3: Disable native freeform task shortcut
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- quickstep/src/com/android/quickstep/TaskShortcutFactory.java
+++ quickstep/src/com/android/quickstep/TaskShortcutFactory.java
@@ -407,10 +407,7 @@ public interface TaskShortcutFactory {
         }
 
         private boolean isAvailable(RecentsViewContainer container) {
-            return Settings.Global.getInt(
-                    container.asContext().getContentResolver(),
-                    Settings.Global.DEVELOPMENT_ENABLE_FREEFORM_WINDOWS_SUPPORT, 0) != 0
-                    && !DesktopModeStatus.canEnterDesktopMode(container.asContext());
+            return false;
         }
     };
 
