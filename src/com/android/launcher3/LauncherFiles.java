1399d521042f83387992a3a4fcf93e8b0e264e64
Abhay Singh Gill <abhaygill017@gmail.com>
2024-12-24 11:08:26 +0530

    Launcher3: Update grid sizes from Pixel launcher
    * This is a minimal import from Pixel launcher
    * Adapt 5+ column grids to be scalable as well
    * Remove 6 column grids for phones as well those are only meant for tablets.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/com/android/launcher3/LauncherFiles.java
+++ src/com/android/launcher3/LauncherFiles.java
@@ -16,7 +16,6 @@ public class LauncherFiles {
     private static final String XML = ".xml";
 
     public static final String LAUNCHER_DB = "launcher.db"; 
-    public static final String LAUNCHER_6_BY_6_DB = "launcher_6_by_6.db";
     public static final String LAUNCHER_6_BY_5_DB = "launcher_6_by_5.db";
     public static final String LAUNCHER_5_BY_7_DB = "launcher_5_by_7.db";
     public static final String LAUNCHER_5_BY_6_DB = "launcher_5_by_6.db";
@@ -36,7 +35,6 @@ public class LauncherFiles {
 
     public static final List<String> GRID_DB_FILES = Collections.unmodifiableList(Arrays.asList(
             LAUNCHER_DB,
-            LAUNCHER_6_BY_6_DB,
             LAUNCHER_6_BY_5_DB,
             LAUNCHER_5_BY_7_DB,
             LAUNCHER_5_BY_6_DB,
