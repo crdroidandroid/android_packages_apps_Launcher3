68136d3e743046d41e68bea24cfb714c9a101b8b
Pranav Vashi <neobuddy89@gmail.com>
2025-01-25 10:33:54 +0530

    Launcher3: Set app drawer opacity to 100
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/com/android/launcher3/Utilities.java
+++ src/com/android/launcher3/Utilities.java
@@ -1130,8 +1130,7 @@ public final class Utilities {
     }
 
     public static int getAllAppsOpacity(Context context) {
-        SharedPreferences prefs = LauncherPrefs.getPrefs(context.getApplicationContext());
-        return prefs.getInt(KEY_APP_DRAWER_OPACITY, 80);
+        return 100;
     }
 
     public static int getBlurRadius(Context context) {
