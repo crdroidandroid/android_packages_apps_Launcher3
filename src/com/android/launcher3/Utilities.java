5078122b0f216eabcd2a6f0dfb6e37c593b692f1
Pranav Vashi <neobuddy89@gmail.com>
2025-02-07 19:26:22 +0530

    Launcher3: Set recent opacity to 100
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/com/android/launcher3/Utilities.java
+++ src/com/android/launcher3/Utilities.java
@@ -1125,8 +1125,7 @@ public final class Utilities {
    }
 
     public static int getRecentsOpacity(Context context) {
-        SharedPreferences prefs = LauncherPrefs.getPrefs(context.getApplicationContext());
-        return prefs.getInt(KEY_RECENTS_OPACITY, 40);
+        return 100;
     }
 
     public static int getAllAppsOpacity(Context context) {
