adb172d5eda59849f4a628acf62f21c0052d654e
Pranav Vashi <neobuddy89@gmail.com>
2025-03-20 10:41:37 +0530

    Launcher3: Check if google app is enabled before running query
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/com/android/launcher3/allapps/search/DefaultSearchAdapterProvider.java
+++ src/com/android/launcher3/allapps/search/DefaultSearchAdapterProvider.java
@@ -24,6 +24,7 @@ import android.view.ViewGroup;
 import com.android.launcher3.BubbleTextView;
 import com.android.launcher3.allapps.AllAppsGridAdapter;
 import com.android.launcher3.model.data.ItemInfo;
+import com.android.launcher3.qsb.QsbContainerView;
 import com.android.launcher3.views.ActivityContext;
 
 /**
@@ -67,9 +68,10 @@ public class DefaultSearchAdapterProvider extends SearchAdapterProvider<Activity
 
     @Override
     public boolean performGoogleSearch(View view, String query) {
-        if (view instanceof View) {
+        String pkgName = QsbContainerView.getSearchWidgetPackageName(view.getContext());
+        if (pkgName != null && view instanceof View) {
             Intent intent = new Intent(Intent.ACTION_WEB_SEARCH);
-            intent.setPackage("com.google.android.googlequicksearchbox");
+            intent.setPackage(pkgName);
             intent.putExtra(SearchManager.QUERY, query);
             return mLauncher.startActivitySafely(view, intent, null) != null;
         }
