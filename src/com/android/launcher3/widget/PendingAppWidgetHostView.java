0e28cb4dde595964f94740984850f3d75f766a44
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-08-13 20:24:34 +0800

    Launcher3: Resolve widget settings icon theme attributes
    08-13 20:21:51.253 17606 17606 W Resources: Drawable com.android.launcher3:drawable/ic_setting has unresolved theme attributes! Consider using Resources.getDrawable(int, Theme) or Context.getDrawable(int).
    08-13 20:21:51.253 17606 17606 W Resources: java.lang.RuntimeException
    08-13 20:21:51.253 17606 17606 W Resources: 	at android.content.res.Resources.getDrawable(Resources.java:926)
    08-13 20:21:51.253 17606 17606 W Resources: 	at com.android.launcher3.widget.PendingAppWidgetHostView.reapplyItemInfo(PendingAppWidgetHostView.java:315)
    08-13 20:21:51.253 17606 17606 W Resources: 	at com.android.launcher3.icons.IconCache$$ExternalSyntheticLambda7.accept(R8$$SyntheticClass:0)
    08-13 20:21:51.253 17606 17606 W Resources: 	at com.android.launcher3.util.CancellableTask$run$1.run(CancellableTask.kt:49)
    
    Change-Id: I1d9e6f91f3581ed121ada9d2e5fea2f591eabf19
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/com/android/launcher3/widget/PendingAppWidgetHostView.java
+++ src/com/android/launcher3/widget/PendingAppWidgetHostView.java
@@ -53,6 +53,7 @@ import android.widget.RemoteViews;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.content.ContextCompat;
 
 import com.android.launcher3.DeviceProfile;
 import com.android.launcher3.Launcher;
@@ -310,7 +311,7 @@ public class PendingAppWidgetHostView extends LauncherAppWidgetHostView
             mSettingIconDrawable = null;
         } else if (isReadyForClickSetup()) {
             mCenterDrawable = info.newIcon(getContext());
-            mSettingIconDrawable = getResources().getDrawable(R.drawable.ic_setting).mutate();
+            mSettingIconDrawable = ContextCompat.getDrawable(getContext(), R.drawable.ic_setting).mutate();
             updateSettingColor(info.bitmap.color);
 
             mDragFlags |= FLAG_DRAW_SETTINGS | FLAG_DRAW_LABEL;
