db814647325936e51d9b73579fb0e01bd2c00a03
Tony Wickham <twickham@google.com>
2025-08-22 15:43:03 -0700

    Send KEYCODE_HOME for home button instead of startActivity directly
    Also updated calls to onBackEvent to match new signature of
    including displayId.
    
    Bug: 381339822
    Flag: com.android.launcher3.home_button_uses_keycode_home
    Test: manual, also OverviewCommandHelperTest
    (cherry picked from https://googleplex-android-review.googlesource.com/q/commit:9ba5fcaffa95621af543be20851806f7dec88183)
    (cherry picked from https://googleplex-android-review.googlesource.com/q/commit:063dd3bcdd0dcbff5be4f53a6f39c4070f76706b)
    Cherrypick-From: https://googleplex-android-review.googlesource.com/q/commit:c16b86ca2e8fbc0b830870b65e7622ba9b8c20cb
    Merged-In: I6e1272e1526e8b136376852f8ff50e3fa68bbede
    Change-Id: I6e1272e1526e8b136376852f8ff50e3fa68bbede

--- quickstep/src/com/android/quickstep/SystemUiProxy.java
+++ quickstep/src/com/android/quickstep/SystemUiProxy.java
@@ -218,6 +218,17 @@ public class SystemUiProxy implements ISystemUiProxy, NavHandle, SafeCloseable {
     }
 
     @Override
+    public void onKeyEvent(int keycode) {
+        if (mSystemUiProxy != null) {
+            try {
+                mSystemUiProxy.onKeyEvent(keycode);
+            } catch (RemoteException e) {
+                Log.w(TAG, "Failed call onKeyEvent", e);
+            }
+        }
+    }
+
+    @Override
     public void onImeSwitcherPressed() {
         if (mSystemUiProxy != null) {
             try {
