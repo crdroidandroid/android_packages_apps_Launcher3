26a493e68f7d648377f37191d294a36dabfcc450
Pranav Vashi <neobuddy89@gmail.com>
2025-08-28 19:15:41 +0530

    Launcher3: Fix recents lag and NPE with third party launchers
    * RecentsView.getScrollScale(...) is being called before RecentsView has initialized its paging arrays.
    * Add an early return when page scrolls aren’t initialized yet.
    
    Log:
    
    time: 1756361134880 msg: java.lang.NullPointerException:
    Attempt to get length of null array stacktrace: java.lang.NullPointerException: Attempt to get length of null array
      at com.android.quickstep.views.RecentsView.getScrollScale(RecentsView.java:7010)
      at com.android.quickstep.AbsSwipeUpHandler.applyScrollAndTransform(AbsSwipeUpHandler.java:2706)
      at com.android.quickstep.AbsSwipeUpHandler.onCurrentShiftUpdated(AbsSwipeUpHandler.java:915)
      at com.android.quickstep.AbsSwipeUpHandler.onApplyWindowInsets(AbsSwipeUpHandler.java:881)
      at android.view.View.dispatchApplyWindowInsets(View.java:12769)
      at android.view.ViewGroup.dispatchApplyWindowInsets(ViewGroup.java:7511)
      at android.view.ViewGroup.newDispatchApplyWindowInsets(ViewGroup.java:7536)
      at android.view.ViewGroup.dispatchApplyWindowInsets(ViewGroup.java:7518)
      at android.view.ViewGroup.newDispatchApplyWindowInsets(ViewGroup.java:7536)
      at android.view.ViewGroup.dispatchApplyWindowInsets(ViewGroup.java:7518)
      at android.view.ViewGroup.newDispatchApplyWindowInsets(ViewGroup.java:7536)
      at android.view.ViewGroup.dispatchApplyWindowInsets(ViewGroup.java:7518)
      at android.view.ViewRootImpl.dispatchApplyInsets(ViewRootImpl.java:3498)
      at android.view.ViewRootImpl.performTraversals(ViewRootImpl.java:3643)
      at android.view.ViewRootImpl.doTraversal(ViewRootImpl.java:3096)
      at android.view.ViewRootImpl$TraversalRunnable.run(ViewRootImpl.java:10669)
      at android.view.Choreographer$CallbackRecord.run(Choreographer.java:1570)
      at android.view.Choreographer$CallbackRecord.run(Choreographer.java:1579)
      at android.view.Choreographer.doCallbacks(Choreographer.java:1179)
      at android.view.Choreographer.doFrame(Choreographer.java:1108)
      at android.view.Choreographer$FrameDisplayEventReceiver.run(Choreographer.java:1553)
      at android.os.Handler.handleCallback(Handler.java:995)
      at android.os.Handler.dispatchMessage(Handler.java:103)  at android.os.Looper.loopOnce(Looper.java:248)
      at android.os.Looper.loop(Looper.java:338)
      at android.app.ActivityThread.main(ActivityThread.java:9068)
      at java.lang.reflect.Method.invoke(Native Method)
      at com.android.internal.os.RuntimeInit$MethodAndArgsCaller.run(RuntimeInit.java:596)
      at com.android.internal.os.ZygoteInit.main(ZygoteInit.java:932)
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- quickstep/src/com/android/quickstep/views/RecentsView.java
+++ quickstep/src/com/android/quickstep/views/RecentsView.java
@@ -6961,8 +6961,8 @@ public abstract class RecentsView<
     }
 
     private void doScrollScale() {
-        if (showAsGrid() || mContainer.getDeviceProfile().isTablet)
-            return;
+        if (showAsGrid() || mContainer.getDeviceProfile().isTablet) return;
+        if (!isPageScrollsInitialized()) return;
 
         //nick@lmo-20231004 if rotating launcher is enabled, rotation works differently
         // There are many edge cases (going from landscape app to recents, rotating in recents etc)
@@ -7007,10 +7007,12 @@ public abstract class RecentsView<
     }
 
     public float getScrollScale(RemoteTargetHandle rth) {
+        if (rth == null || showAsGrid() || mContainer.getDeviceProfile().isTablet) return 1f;
+        if (!isPageScrollsInitialized()) return 1f;
         int childCount = Math.min(mPageScrolls.length, getChildCount());
         for (int i = 0; i < childCount; i++) {
             View child = getChildAt(i);
-            if (!(child instanceof TaskView && !showAsGrid())) continue;
+            if (!(child instanceof TaskView)) continue;
             TaskView tv = (TaskView) child;
             TransformParams params = rth.getTransformParams();
             RemoteAnimationTargets targets = params.getTargetSet();
