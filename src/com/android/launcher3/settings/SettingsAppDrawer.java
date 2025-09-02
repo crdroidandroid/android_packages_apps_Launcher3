0f91ec11a13cf1cf08beabe40b8cbe50104713d7
Pranav Vashi <neobuddy89@gmail.com>
2025-09-02 19:19:16 +0530

    Launcher3: Setup material expressive design for preferences
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/com/android/launcher3/settings/SettingsAppDrawer.java
+++ src/com/android/launcher3/settings/SettingsAppDrawer.java
@@ -29,7 +29,6 @@ import android.view.View;
 
 import androidx.annotation.Nullable;
 import androidx.annotation.VisibleForTesting;
-import androidx.core.view.WindowCompat;
 import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
@@ -52,6 +51,7 @@ import com.android.launcher3.util.Executors;
 import com.android.launcher3.util.SettingsCache;
 
 import com.android.settingslib.collapsingtoolbar.CollapsingToolbarBaseActivity;
+import com.android.settingslib.widget.SettingsBasePreferenceFragment;
 
 /**
  * Settings activity for Launcher.
@@ -73,8 +73,6 @@ public class SettingsAppDrawer extends CollapsingToolbarBaseActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.settings_activity);
-
-        WindowCompat.setDecorFitsSystemWindows(getWindow(), false);
 
         Intent intent = getIntent();
 
@@ -145,7 +143,7 @@ public class SettingsAppDrawer extends CollapsingToolbarBaseActivity
     /**
      * This fragment shows the launcher preferences.
      */
-    public static class AppDrawerSettingsFragment extends PreferenceFragmentCompat implements
+    public static class AppDrawerSettingsFragment extends SettingsBasePreferenceFragment implements
             SettingsCache.OnChangeListener {
 
         private boolean mRestartOnResume = false;
