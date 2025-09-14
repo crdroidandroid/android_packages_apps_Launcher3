e4ee3b2848d4b780a063f7094ff8121e5efad445
Pranav Vashi <neobuddy89@gmail.com>
2025-09-14 14:13:09 +0530

    Launcher3: Ensure surfaces/animators are cleaned in onActivityDestroyed()
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- quickstep/src/com/android/launcher3/QuickstepTransitionManager.java
+++ quickstep/src/com/android/launcher3/QuickstepTransitionManager.java
@@ -1316,6 +1316,7 @@ public class QuickstepTransitionManager implements OnDeviceProfileChangeListener
         mLauncherOpenTransition = null;
         mWallpaperOpenTransitionRunner = null;
         if (mBackAnimationController != null) {
+            mBackAnimationController.cleanupForDestroy();
             mBackAnimationController.unregisterBackCallbacks();
             mBackAnimationController.unregisterComponentCallbacks();
             mBackAnimationController = null;
