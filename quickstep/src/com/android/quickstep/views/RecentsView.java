17af3ab50b4de3fbe64511cb5e9397777d43ce0d
Pranav Vashi <neobuddy89@gmail.com>
2025-10-12 02:03:56 +0530

    Launcher3: RecentsView: Reuse a shared Matrix to reduce GC
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- quickstep/src/com/android/quickstep/views/RecentsView.java
+++ quickstep/src/com/android/quickstep/views/RecentsView.java
@@ -546,6 +546,7 @@ public abstract class RecentsView<
     protected final RectF mTempRectF = new RectF();
     private final PointF mTempPointF = new PointF();
     private final Matrix mTempMatrix = new Matrix();
+    private final Matrix mAnimMatrix = new Matrix();
     private final float[] mTempFloat = new float[1];
     private final List<OnScrollChangedListener> mScrollListeners = new ArrayList<>();
 
@@ -1444,7 +1445,6 @@ public abstract class RecentsView<
             ValueAnimator appAnimator = ValueAnimator.ofFloat(0, 1);
             appAnimator.setDuration(RECENTS_LAUNCH_DURATION);
             appAnimator.setInterpolator(ACCELERATE_DECELERATE);
-            final Matrix matrix = new Matrix();
             appAnimator.addUpdateListener(valueAnimator -> {
                 float percent = valueAnimator.getAnimatedFraction();
                 SurfaceTransaction transaction = new SurfaceTransaction();
@@ -1455,11 +1455,12 @@ public abstract class RecentsView<
                             + app.screenSpaceBounds.left * percent;
                     float dy = mContainer.getDeviceProfile().heightPx * (1 - percent) / 2
                             + app.screenSpaceBounds.top * percent;
-                    matrix.setScale(percent, percent);
-                    matrix.postTranslate(dx, dy);
+                    mAnimMatrix.reset();
+                    mAnimMatrix.setScale(percent, percent);
+                    mAnimMatrix.postTranslate(dx, dy);
                     transaction.forSurface(app.leash)
                             .setAlpha(percent)
-                            .setMatrix(matrix);
+                            .setMatrix(mAnimMatrix);
                 }
                 surfaceApplier.scheduleApply(transaction);
             });
@@ -6131,6 +6132,7 @@ public abstract class RecentsView<
             final TransformParams params = remoteTargetHandle.getTransformParams();
             if (RecentsWindowFlags.Companion.getEnableOverviewInWindow()) {
                 params.setHomeBuilderProxy((builder, app, transformParams) -> {
+                    mTmpMatrix.reset();
                     mTmpMatrix.setScale(
                             1f, 1f, app.localBounds.exactCenterX(), app.localBounds.exactCenterY());
                     builder.setMatrix(mTmpMatrix).setAlpha(1f).setShow();
