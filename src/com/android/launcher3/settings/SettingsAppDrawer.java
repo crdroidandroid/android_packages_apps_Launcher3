f055a0a9dfabbd1bc735f724da1c69033dfc983e
Dmitrii <bankersenator@gmail.com>
2025-10-17 03:36:11 +0000

    Launcher3: Dark text on appdrawer toggle
    Signed-off-by: Dmitrii <bankersenator@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/com/android/launcher3/settings/SettingsAppDrawer.java
+++ src/com/android/launcher3/settings/SettingsAppDrawer.java
@@ -115,7 +115,8 @@ public class SettingsAppDrawer extends CollapsingToolbarBaseActivity
     @Override
     public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
         if (LauncherPrefs.ALL_APPS_SEARCH_PLACEMENT.getSharedPrefKey().equals(key) ||
-                LauncherPrefs.DRAWER_SCROLLBAR.getSharedPrefKey().equals(key)) {
+                LauncherPrefs.DRAWER_SCROLLBAR.getSharedPrefKey().equals(key) ||
+                LauncherPrefs.ALL_APPS_DARK_TEXT.getSharedPrefKey().equals(key)) {
             LauncherAppState.INSTANCE.executeIfCreated(app -> app.setNeedsRestart());
         }
     }
