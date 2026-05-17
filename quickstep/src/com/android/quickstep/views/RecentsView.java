897c145697797d542d8a555ef2970cca7beb7428
Pranav Vashi <neobuddy89@gmail.com>
2026-05-17 15:47:14 +0530

    Launcher3: Add default style for recents view animation
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- quickstep/src/com/android/quickstep/views/RecentsView.java
+++ quickstep/src/com/android/quickstep/views/RecentsView.java
@@ -1285,11 +1285,11 @@ public abstract class RecentsView<
         return true;
     }
 
-    private String mRecentsStyle = "stock"; 
+    private String mRecentsStyle = "default"; 
 
     public void updateOverlapState() {
         mRecentsStyle = LauncherPrefs.RECENTS_STYLE.get(getContext());
-        mEnableOverlap = !mRecentsStyle.equals("stock");
+        mEnableOverlap = !mRecentsStyle.equals("default") && !mRecentsStyle.equals("stock");
     }
 
     @Override
@@ -5027,7 +5027,7 @@ public abstract class RecentsView<
                         .setScroll(getScrollOffset()));
         setImportantForAccessibility(isModal() ? IMPORTANT_FOR_ACCESSIBILITY_NO
                 : IMPORTANT_FOR_ACCESSIBILITY_AUTO);
-        if (!mEnableOverlap) {
+        if (mRecentsStyle.equals("stock")) {
             doScrollScale();
         }
     }
@@ -6967,7 +6967,7 @@ public abstract class RecentsView<
         super.onScrollChanged(l, t, oldl, oldt);
         dispatchScrollChanged();
         updatePageOffsets();
-        if (!mEnableOverlap) {
+        if (mRecentsStyle.equals("stock")) {
             doScrollScale();
         }
     }
